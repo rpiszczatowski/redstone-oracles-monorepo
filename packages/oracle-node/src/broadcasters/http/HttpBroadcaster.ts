830df509136aa828b0d331bf004259b03d8f4ce2
cehali <cezary.haliniarz@gmail.com>
2022-07-20 13:35:38 +0200

    feat: add redstone node to monorepo

---
+++ packages/oracle-node/src/broadcasters/http/HttpBroadcaster.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { Broadcaster } from "../Broadcaster";
+import { PriceDataSigned, SignedPricePackage } from "../../types";
+import { Consola } from "consola";
+import { stringifyError } from "../../utils/error-stringifier";
+
+const logger = require("../../utils/logger")("HttpBroadcaster") as Consola;
+
+// TODO: add timeout to broadcasting
+
+export class HttpBroadcaster implements Broadcaster {
+  constructor(private readonly broadcasterURLs: string[]) {}
+
+  async broadcast(prices: PriceDataSigned[]): Promise<void> {
+    const promises = this.broadcasterURLs.map((url) => {
+      logger.info(`Posting prices to ${url}`);
+      return axios
+        .post(url + "/prices", prices)
+        .then(() => logger.info(`Broadcasting to ${url} completed`))
+        .catch((e) =>
+          logger.error(`Broadcasting to ${url} failed: ${stringifyError(e)}`)
+        );
+    });
+
+    await Promise.allSettled(promises);
+  }
+
+  async broadcastPricePackage(
+    signedData: SignedPricePackage,
+    providerAddress: string
+  ): Promise<void> {
+    const body = {
+      signerAddress: signedData.signerAddress,
+      liteSignature: signedData.liteSignature,
+      provider: providerAddress,
+      ...signedData.pricePackage, // unpacking prices and timestamp
+    };
+
+    const promises = this.broadcasterURLs.map((url) => {
+      logger.info(`Posting pacakages to ${url}`);
+      return axios
+        .post(url + "/packages", body)
+        .then(() => logger.info(`Broadcasting package to ${url} completed`))
+        .catch((e) =>
+          logger.error(
+            `Broadcasting package to ${url} failed: ${stringifyError(e)}`
+          )
+        );
+    });
+
+    await Promise.allSettled(promises);
+  }
+}
---
+++ packages/oracle-node/src/signers/PriceSignerService.ts
@@ -0,0 +1,81 @@
+import { Consola } from "consola";
+import _ from "lodash";
+import EvmPriceSigner from "./EvmPriceSigner";
+import {
+  PriceDataBeforeSigning,
+  PriceDataSigned,
+  SignedPricePackage,
+} from "../types";
+import { trackStart, trackEnd } from "../utils/performance-tracker";
+
+const logger = require("../utils/logger")("ArweaveService") as Consola;
+
+interface PriceSignerConfig {
+  version: string;
+  evmChainId: number;
+  ethereumPrivateKey: string;
+}
+
+// Business service that supplies signing operations required by Redstone-Node
+export default class PriceSignerService {
+  private evmSigner: EvmPriceSigner;
+  private ethereumPrivateKey: string;
+
+  constructor(config: PriceSignerConfig) {
+    this.evmSigner = new EvmPriceSigner(config.version, config.evmChainId);
+    this.ethereumPrivateKey = config.ethereumPrivateKey;
+  }
+
+  async signPrices(
+    prices: PriceDataBeforeSigning[]
+  ): Promise<PriceDataSigned[]> {
+    const signingTrackingId = trackStart("signing");
+    const signedPrices: PriceDataSigned[] = [];
+
+    try {
+      for (const price of prices) {
+        logger.info(`Signing price: ${price.id}`);
+        const signedPrice = await this.signSinglePrice(price);
+        signedPrices.push(signedPrice);
+      }
+      return signedPrices;
+    } finally {
+      trackEnd(signingTrackingId);
+    }
+  }
+
+  async signSinglePrice(
+    price: PriceDataBeforeSigning
+  ): Promise<PriceDataSigned> {
+    logger.info(`Signing price with evm signer: ${price.id}`);
+    const packageWithSinglePrice = this.evmSigner.signPricePackage(
+      {
+        prices: [_.pick(price, ["symbol", "value"])],
+        timestamp: price.timestamp,
+      },
+      this.ethereumPrivateKey
+    );
+
+    return {
+      ...price,
+      // evmSignature: packageWithSinglePrice.signature,
+      liteEvmSignature: packageWithSinglePrice.liteSignature,
+    };
+  }
+
+  signPricePackage(prices: PriceDataSigned[]): SignedPricePackage {
+    if (prices.length === 0) {
+      throw new Error("Price package should contain at least one price");
+    }
+
+    const pricePackage = {
+      timestamp: prices[0].timestamp,
+      prices: prices.map((p) => _.pick(p, ["symbol", "value"])),
+    };
+
+    return this.evmSigner.signPricePackage(
+      pricePackage,
+      this.ethereumPrivateKey
+    );
+  }
+}
