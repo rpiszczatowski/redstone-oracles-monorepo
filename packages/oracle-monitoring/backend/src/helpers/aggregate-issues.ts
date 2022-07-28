46a6511aa31bc6754d9ec3d8a260b899e7510193
cehali <cezary.haliniarz@gmail.com>
2022-07-28 10:48:50 +0200

    feat: add oracle monitoring to monorepo

---
+++ packages/oracle-monitoring/backend/src/helpers/aggregate-issues.ts
@@ -0,0 +1,47 @@
+import { Issue } from "../models/issue";
+
+export const aggregateIssuesByAttribute = (
+  fromTimestamp: number,
+  attributeName: keyof Issue
+) => {
+  return Issue.aggregate([
+    {
+      $match: {
+        timestamp: {
+          $gte: fromTimestamp,
+        },
+      },
+    },
+    {
+      $group: {
+        _id: `$${attributeName}`,
+        errors: {
+          $sum: { $cond: [{ $eq: ["$level", "ERROR"] }, 1, 0] },
+        },
+        warnings: {
+          $sum: { $cond: [{ $eq: ["$level", "WARNING"] }, 1, 0] },
+        },
+        "data-feed-failed": {
+          $sum: { $cond: [{ $eq: ["$type", "data-feed-failed"] }, 1, 0] },
+        },
+        "one-source-failed": {
+          $sum: { $cond: [{ $eq: ["$type", "one-source-failed"] }, 1, 0] },
+        },
+        "timestamp-diff": {
+          $sum: { $cond: [{ $eq: ["$type", "timestamp-diff"] }, 1, 0] },
+        },
+      },
+    },
+    {
+      $project: {
+        errors: "$errors",
+        warnings: "$warnings",
+        groupedByType: {
+          "data-feed-failed": "$data-feed-failed",
+          "one-source-failed": "$one-source-failed",
+          "timestamp-diff": "$timestamp-diff",
+        },
+      },
+    },
+  ]);
+};
