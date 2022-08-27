ed428a75e3511f73d7184fb4b4e4a25128986ba7
hatskier <suvorovalex1997@gmail.com>
2022-08-27 15:44:34 +0200

    chore: small updated in the manifest generation scripts

--- packages/oracle-node/tools/manifest/generate-main-manifest.ts
+++ packages/oracle-node/tools/manifest/generate-main-manifest.ts
@@ -6,6 +6,10 @@ import {
 } from "../common/paths";
 import { generateManifest } from "../common/manifest-utils";
 import { Manifest, TokensConfig } from "../../src/types";
+import popularTokens from "./popular-tokens.json";
+
+const MAX_DEVIATION_FOR_POPULAR_TOKENS = 25;
+const MAX_DEVIATION_FOR_UNPOPULAR_TOKENS = 80;
 
 main();
 
@@ -23,6 +27,7 @@ function main() {
       } else {
         tokens[tokenName] = {
           source: [sourceId],
+          maxPriceDeviationPercent: getMaxDeviationPercent(tokenName),
         };
       }
     }
@@ -52,4 +57,10 @@ function readSingleSourceManifests() {
   }
 
   return manifests;
+}
+
+function getMaxDeviationPercent(symbol: string): number {
+  return popularTokens.includes(symbol)
+    ? MAX_DEVIATION_FOR_POPULAR_TOKENS
+    : MAX_DEVIATION_FOR_UNPOPULAR_TOKENS;
 }
