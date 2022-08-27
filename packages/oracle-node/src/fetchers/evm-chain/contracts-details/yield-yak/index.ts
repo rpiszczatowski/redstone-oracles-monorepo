fbcdbd3b62f4465bafbc607b6b0819d847033649
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2022-08-27 15:52:11 +0200

    feat: add sav2 token (#34)

--- packages/oracle-node/src/fetchers/evm-chain/contracts-details/yield-yak/index.ts
+++ packages/oracle-node/src/fetchers/evm-chain/contracts-details/yield-yak/index.ts
@@ -1,6 +1,13 @@
-import YieldYakAbi from "./YieldYak.abi.json";
+import YieldYakAVAXAbi from "./YieldYakAVAX.abi.json";
+import YieldYakSAVAXAbi from "./YieldYakSAVAX.abi.json";
 
 export const yieldYakContractDetails = {
-  address: "0xaAc0F2d0630d1D09ab2B5A400412a4840B866d95",
-  abi: YieldYakAbi,
+  YYAV3SA1: {
+    address: "0xaAc0F2d0630d1D09ab2B5A400412a4840B866d95",
+    abi: YieldYakAVAXAbi,
+  },
+  SAV2: {
+    address: "0xd0F41b1C9338eB9d374c83cC76b684ba3BB71557",
+    abi: YieldYakSAVAXAbi,
+  },
 };
