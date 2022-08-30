ad351dab1b638c6f203fb5d9069387c10e001581
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2022-08-30 15:19:00 +0200

    feat: replace monitoring with nest version (#36)
    * chore: remove old monitoring backend
    
    * feat: update monitoring nest version

---
+++ packages/oracle-monitoring/backend/src/modules/issues/issues.schema.ts
@@ -0,0 +1,40 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+export type IssueDocument = Issue & Document;
+
+@Schema()
+export class Issue {
+  @Prop({ required: true })
+  timestamp: number;
+
+  @Prop({ required: true })
+  type:
+    | "timestamp-diff"
+    | "invalid-signature"
+    | "one-source-failed"
+    | "data-feed-failed";
+
+  @Prop()
+  symbol: string;
+
+  @Prop({ required: true })
+  level: "ERROR" | "WARNING";
+
+  @Prop({ required: true })
+  dataFeedId: string;
+
+  @Prop()
+  evmSignerAddress: string;
+
+  @Prop()
+  url: string;
+
+  @Prop()
+  comment: string;
+
+  @Prop()
+  timestampDiffMilliseconds: number;
+}
+
+export const IssueSchema = SchemaFactory.createForClass(Issue);
