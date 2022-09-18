cd06b5c3d850024215ebef96839615ae8ac7a792
hatskier <suvorovalex1997@gmail.com>
2022-09-18 15:21:39 +0200

    chore: hardhat project configured

---
+++ packages/eth-contracts/contracts/DisputeResolutionEngine.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+// TODO: implement
+contract DisputeResolutionEngine is OwnableUpgradeable {
+
+}
---
+++ packages/eth-contracts/test/integration.test.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Integration tests", function () {
+  it("Hehe", () => {
+    expect(2 + 2).to.equal(4);
+  });
+});
