6f2cbc8300e15c7d2a526e0513d21e491bb7a94b
hatskier <suvorovalex1997@gmail.com>
2022-10-17 13:29:15 +0200

    fix: tests in redstone-sdk adjusted

--- packages/sdk/test/index.test.ts
+++ packages/sdk/test/index.test.ts
@@ -17,7 +17,7 @@ describe("SDK tests", () => {
 
   test("Should properly get oracle registry state", async () => {
     const state = await getOracleRegistryState();
-    expect(Object.keys(state.dataServices).length).toBe(8);
+    expect(Object.keys(state.dataServices).length).toBe(7);
     expect(state.dataServices).toHaveProperty("redstone-stocks-demo");
   });
 
