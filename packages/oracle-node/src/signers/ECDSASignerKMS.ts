e2e40565b59317316bb6961759314806faed5611
hatskier <suvorovalex1997@gmail.com>
2022-11-09 14:24:34 +0100

    feat: initial implementation of KMS signer

--- packages/oracle-node/src/signers/ECDSASignerKMS.ts
+++ packages/oracle-node/src/signers/ECDSASignerKMS.ts
@@ -1,14 +1,24 @@
 import { Signature } from "ethers";
-import { SigningKey } from "ethers/lib/utils";
+import { hexlify, splitSignature } from "ethers/lib/utils";
 import { ECDSASignerBase } from "./ECDSASignerBase";
+import KMS from "aws-sdk/clients/kms";
 
 export default class ECDSASignerKMS extends ECDSASignerBase {
   constructor(private readonly kmsARN: string) {
     super();
   }
 
-  // TODO: implement request to KMS
   async signHash(hash: Uint8Array): Promise<Signature> {
-    return new SigningKey("0x1234").signDigest(hash);
+    const kms = new KMS({});
+    const signingResult = await kms
+      .sign({
+        // TOOD: move it to config.ts
+        KeyId: process.env.AWS_KMS_KEY_ID || "",
+        Message: hexlify(hash),
+        SigningAlgorithm: "ECDSA_SHA_512",
+      })
+      .promise();
+    const signatureHex = signingResult.Signature!.toString("hex");
+    return splitSignature(signatureHex);
   }
 }
