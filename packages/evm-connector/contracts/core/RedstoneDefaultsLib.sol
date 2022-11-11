b9ddde1157e73380a0585319688f7b7f5ea0e05a
Alex Suvorov <Suvorovalex1997@gmail.com>
2022-11-11 22:09:52 +0100

    feat: support of duplicated data feeds in evm-connector (#122)
    * feat: implemented  with tests
    
    * build: @redstone-finance/evm-connector@0.0.10
    
    * chore: timestamp validation adjusted
    
    * build: @redstone-finance/evm-connector@0.0.11
    
    * chore: fixes after review
    
    * test: added tests for the redstone defaults lib
    
    * chore: minor code refactoring in timestamp validation function
    
    * chore: fixes after Kuba's review

--- packages/evm-connector/contracts/core/RedstoneDefaultsLib.sol
+++ packages/evm-connector/contracts/core/RedstoneDefaultsLib.sol
@@ -23,13 +23,12 @@ library RedstoneDefaultsLib {
     // That's why we add MAX_BLOCK_TIMESTAMP_DELAY
     // and allow data "from future" but with a small delay
     uint256 receivedTimestampSeconds = receivedTimestampMilliseconds / 1000;
-    uint256 timestampDiffSeconds = block.timestamp - receivedTimestampSeconds;
-    bool isFromFuture = block.timestamp < receivedTimestampSeconds;
 
-    if ((block.timestamp + DEFAULT_MAX_DATA_TIMESTAMP_AHEAD_SECONDS) < receivedTimestampSeconds) {
-      revert TimestampFromTooLongFuture(receivedTimestampSeconds, block.timestamp);
-    }
-    if (!isFromFuture && timestampDiffSeconds > DEFAULT_MAX_DATA_TIMESTAMP_DELAY_SECONDS) {
+    if (block.timestamp < receivedTimestampSeconds) {
+      if ((receivedTimestampSeconds - block.timestamp) > DEFAULT_MAX_DATA_TIMESTAMP_AHEAD_SECONDS) {
+        revert TimestampFromTooLongFuture(receivedTimestampSeconds, block.timestamp);
+      }
+    } else if ((block.timestamp - receivedTimestampSeconds) > DEFAULT_MAX_DATA_TIMESTAMP_DELAY_SECONDS) {
       revert TimestampIsTooOld(receivedTimestampSeconds, block.timestamp);
     }
   }
