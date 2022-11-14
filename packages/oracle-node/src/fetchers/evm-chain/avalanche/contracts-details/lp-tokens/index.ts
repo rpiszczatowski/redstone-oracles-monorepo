1476c928a8799b161494adfc2edbcec8d0fd0dd8
cehali <cezary.haliniarz@gmail.com>
2022-11-14 14:26:36 +0100

    feat: add TJ and PNG LP tokens

--- packages/oracle-node/src/fetchers/evm-chain/avalanche/contracts-details/lp-tokens/index.ts
+++ packages/oracle-node/src/fetchers/evm-chain/avalanche/contracts-details/lp-tokens/index.ts
@@ -14,4 +14,24 @@ export const lpTokensContractsDetails = {
     address: "0xe3bA3d5e3F98eefF5e9EDdD5Bd20E476202770da",
     abi: PangolinLpAbi,
   },
+  TJ_USDT_WAVAX_LP: {
+    address: "0xbb4646a764358ee93c2a9c4a147d5aDEd527ab73",
+    abi: TraderJoeLpAbi,
+  },
+  TJ_sAVAX_WAVAX_LP: {
+    address: "0x4b946c91C2B1a7d7C40FB3C130CdfBaf8389094d",
+    abi: TraderJoeLpAbi,
+  },
+  PNG_WETH_WAVAX_LP: {
+    address: "0x7c05d54fc5CB6e4Ad87c6f5db3b807C94bB89c52",
+    abi: PangolinLpAbi,
+  },
+  TJ_WETH_WAVAX_LP: {
+    address: "0xFE15c2695F1F920da45C30AAE47d11dE51007AF9",
+    abi: TraderJoeLpAbi,
+  },
+  TJ_BTC_WAVAX_LP: {
+    address: "0x2fD81391E30805Cc7F2Ec827013ce86dc591B806",
+    abi: TraderJoeLpAbi,
+  },
 };
