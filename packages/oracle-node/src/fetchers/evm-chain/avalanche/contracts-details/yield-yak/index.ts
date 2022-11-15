ca1fae3c00cbd878bfbad84a78c91773f8e9ceb0
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2022-11-15 18:28:04 +0100

    feat: rename lp tokens, add tokens to fetch in config (#137)

--- packages/oracle-node/src/fetchers/evm-chain/avalanche/contracts-details/yield-yak/index.ts
+++ packages/oracle-node/src/fetchers/evm-chain/avalanche/contracts-details/yield-yak/index.ts
@@ -7,29 +7,36 @@ export const yieldYakContractsDetails = {
   YYAV3SA1: {
     address: "0xaAc0F2d0630d1D09ab2B5A400412a4840B866d95",
     abi: YieldYakAaveAVAXAbi,
+    tokenToFetch: "AVAX",
   },
-  YY_PTP_SAVAX_FT: {
+  YY_PTP_sAVAX_FT: {
     address: "0xb8f531c0d3c53B1760bcb7F57d87762Fd25c4977",
     abi: YYPangolinSAVAXAbi,
+    tokenToFetch: "sAVAX",
   },
-  YY_TJ_WAVAX_USDC_LP: {
+  YY_TJ_AVAX_USDC_LP: {
     address: "0xDEf94a13fF31FB6363f1e03bF18fe0F59Db83BBC",
     abi: YYTraderJoeLPTokenAbi,
+    tokenToFetch: "TJ_AVAX_USDC_LP",
   },
-  YY_PNG_WAVAX_USDC_LP: {
+  YY_PNG_AVAX_USDC_LP: {
     address: "0xC0cd58661b68e10b49D3Bec4bC5E44e7A7c20656",
     abi: YYPangolinLPTokenAbi,
+    tokenToFetch: "PNG_AVAX_USDC_LP",
   },
-  YY_PNG_WETH_WAVAX_LP: {
+  YY_PNG_AVAX_ETH_LP: {
     address: "0xFCD2050E213cC54db2c9c99632AC870574FbC261",
     abi: YYPangolinLPTokenAbi,
+    tokenToFetch: "PNG_AVAX_ETH_LP",
   },
-  YY_TJ_sAVAX_WAVAX_LP: {
+  YY_TJ_AVAX_sAVAX_LP: {
     address: "0x22EDe03f1115666CF05a4bAfafaEe8F43D42cD56",
     abi: YYTraderJoeLPTokenAbi,
+    tokenToFetch: "TJ_AVAX_sAVAX_LP",
   },
-  YY_TJ_WETH_WAVAX_LP: {
+  YY_TJ_AVAX_ETH_LP: {
     address: "0x5219558ee591b030E075892acc41334A1694fd8A",
     abi: YYTraderJoeLPTokenAbi,
+    tokenToFetch: "TJ_AVAX_ETH_LP",
   },
 };
