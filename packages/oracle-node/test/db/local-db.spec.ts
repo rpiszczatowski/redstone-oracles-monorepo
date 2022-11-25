bb89b0c372bcfe7d5380d2e8a82f0443934a1b3c
hatskier <suvorovalex1997@gmail.com>
2022-11-25 20:53:54 +0100

    test: added more local db tests

--- packages/oracle-node/test/db/local-db.spec.ts
+++ packages/oracle-node/test/db/local-db.spec.ts
@@ -2,6 +2,7 @@ import {
   clearPricesSublevel,
   closeLocalLevelDB,
   getPrices,
+  PriceValuesInLocalDB,
   savePrices,
 } from "../../src/db/local-db";
 import { PriceDataAfterAggregation } from "../../src/types";
@@ -30,6 +31,12 @@ const prices: PriceDataAfterAggregation[] = [
     value: 42,
   },
 ];
+
+jest.setTimeout(120000); // This file has a long-running test
+
+const mockCurrentTimestamp = (newCurrentTimestamp: number) => {
+  jest.useFakeTimers().setSystemTime(new Date(newCurrentTimestamp));
+};
 
 describe("Local DB", () => {
   beforeEach(async () => {
@@ -83,7 +90,7 @@ describe("Local DB", () => {
     });
 
     const newCurrentTimestamp = Date.now() + PRICES_TTL_MILLISECONDS + 1;
-    jest.useFakeTimers().setSystemTime(new Date(newCurrentTimestamp));
+    mockCurrentTimestamp(newCurrentTimestamp);
 
     const newerPrices = prices.map((p) => ({
       ...p,
@@ -97,6 +104,71 @@ describe("Local DB", () => {
     });
   });
 
-  // TODO: implement
-  it("should correctly simulate adding and clearing data of 2K assets for 30 minutes", async () => {});
+  it("should correctly simulate adding and clearing data of 2K assets for 20 minutes", async () => {
+    const testAssetsCount = 2000;
+    const testingRangeLengthMilliseconds = 20 * 60 * 1000; // 20 minutes
+    const startTimestamp = Date.now();
+    const endTimestamp = startTimestamp + testingRangeLengthMilliseconds;
+    const interval = 10000; // 10 seconds
+
+    const getTestSymbol = (assetIndex: number) => `SYMBOL-${assetIndex}`;
+
+    const preparePrices = (timestamp: number): PriceDataAfterAggregation[] => {
+      const prices: PriceDataAfterAggregation[] = [];
+      for (let assetIndex = 0; assetIndex < testAssetsCount; assetIndex++) {
+        const symbol = getTestSymbol(assetIndex);
+        prices.push({
+          ...defaultPriceProps,
+          symbol,
+          value: assetIndex,
+          timestamp,
+        });
+      }
+      return prices;
+    };
+
+    // This function verifies if there are all required prices
+    // But there are no "old" prices
+    const testPricesFromDB = (
+      pricesFromDB: PriceValuesInLocalDB,
+      timestamp: number,
+      expectedPricesCountPerAsset: number
+    ) => {
+      // We check values only for few assets to speed up tests
+      for (const assetIndex of [0, 42, 1500]) {
+        const symbol = getTestSymbol(assetIndex);
+        expect(pricesFromDB).toHaveProperty(symbol);
+        expect(pricesFromDB[symbol].length).toBe(expectedPricesCountPerAsset);
+        for (const priceFromDB of pricesFromDB[symbol]) {
+          expect(priceFromDB.timestamp).toBeGreaterThanOrEqual(
+            timestamp - PRICES_TTL_MILLISECONDS
+          );
+          expect(priceFromDB.value).toBe(assetIndex);
+        }
+      }
+    };
+
+    // Running the simulation with tests
+    let iterationIndex = 0;
+    for (
+      let timestamp = startTimestamp;
+      timestamp <= endTimestamp;
+      timestamp += interval
+    ) {
+      mockCurrentTimestamp(timestamp);
+      console.log(
+        `Testing price clearing in local db. ` +
+          `Iteration index: ${iterationIndex}/120. Mock timestamp: ${timestamp}`
+      );
+      const prices = preparePrices(timestamp);
+      await savePrices(prices);
+      const pricesFromDB = await getPrices(prices.map((p) => p.symbol));
+      const expectedPricesCountPerAsset = Math.min(
+        iterationIndex + 1,
+        Math.floor(PRICES_TTL_MILLISECONDS / interval)
+      );
+      testPricesFromDB(pricesFromDB, timestamp, expectedPricesCountPerAsset);
+      iterationIndex++;
+    }
+  });
 });
