b34f1fdf33867b03712840bc861f4c9b0bc41436
hatskier <suvorovalex1997@gmail.com>
2022-11-30 16:29:29 +0100

    chore: adjustments after review

--- packages/cache-service/src/data-packages/data-packages.controller.ts
+++ packages/cache-service/src/data-packages/data-packages.controller.ts
@@ -26,8 +26,8 @@ export interface GetLatestDataPackagesQuery {
 }
 
 export interface GetDataPackagesStatsQuery {
-  "from-timestamp": number;
-  "to-timestamp": number;
+  "from-timestamp": string;
+  "to-timestamp"?: string;
   "api-key": string;
 }
 
