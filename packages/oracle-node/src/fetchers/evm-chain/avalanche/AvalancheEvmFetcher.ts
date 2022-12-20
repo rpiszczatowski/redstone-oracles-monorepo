91e67f87b8ce3b626e486c1af78a18a413af2a46
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2022-12-20 10:55:21 +0100

    feat: add GLP token (#231)

--- packages/oracle-node/src/fetchers/evm-chain/avalanche/AvalancheEvmFetcher.ts
+++ packages/oracle-node/src/fetchers/evm-chain/avalanche/AvalancheEvmFetcher.ts
@@ -8,17 +8,20 @@ import { mooTokensContractsDetails } from "./contracts-details/moo-joe";
 import { MulticallParsedResponses, PricesObj } from "../../../types";
 import { extractPrice } from "./extract-price";
 import { oracleAdaptersContractsDetails } from "./contracts-details/oracle-adapters";
+import { glpManagerContractsDetails } from "./contracts-details/glp-manager";
 
 export type YieldYakDetailsKeys = keyof typeof yieldYakContractsDetails;
 export type LpTokensDetailsKeys = keyof typeof lpTokensContractsDetails;
 export type MooJoeTokensDetailsKeys = keyof typeof mooTokensContractsDetails;
 export type OracleAdaptersDetailsKeys =
   keyof typeof oracleAdaptersContractsDetails;
+export type GlpManagerDetailsKeys = keyof typeof glpManagerContractsDetails;
 
 export const yyTokenIds = Object.keys(yieldYakContractsDetails);
 export const lpTokensIds = Object.keys(lpTokensContractsDetails);
 export const mooTokens = Object.keys(mooTokensContractsDetails);
 export const oracleAdaptersTokens = Object.keys(oracleAdaptersContractsDetails);
+export const glpToken = Object.keys(glpManagerContractsDetails);
 
 const MUTLICALL_CONTRACT_ADDRESS = "0x8755b94F88D120AB2Cc13b1f6582329b067C760d";
 
