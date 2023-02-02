4402e132fa789df76c5e6b74f3c8ff062073b462
rucinpk <rucinpk@gmail.com>
2023-02-02 17:19:18 +0100

    refactor of historical fetching script

--- packages/oracle-node/tools/uniswap-v3/get-historical-prices.ts
+++ packages/oracle-node/tools/uniswap-v3/get-historical-prices.ts
@@ -1,6 +1,10 @@
-import { range } from "../utils";
 import fs from "fs";
 import { UniswapV3FetcherHistorical } from "../../src/fetchers/uniswap-v3/historical/UniswapV3FetcherHistorical";
+import { saveJSON } from "../common/fs-utils";
+
+const UNISWAP_PRICES_FILEPATH = "uniswap-prices.json";
+const TIMESTAMPS_TO_FETCH_FILEPATH = "timestamps-to-fetch.json";
+const PROMISE_BATCH_SIZE = 100;
 
 Promise.all = function promiseAllIterative(values: any): any {
   return new Promise((resolve, reject) => {
@@ -27,16 +31,16 @@ interface PriceWithTimestamp {
 }
 
 const getHistoricalPrices = async () => {
-  let allPrices = readPrices("final_prices_array.json");
-  let fetchedPrices = readPrices("uniswap-prices.json");
+  const timestampsToFetch = readPrices(TIMESTAMPS_TO_FETCH_FILEPATH);
+  const fetchedPrices = readPrices(UNISWAP_PRICES_FILEPATH);
 
-  let allTimestamps = new Set();
+  const allTimestamps = new Set();
 
-  for (const price of allPrices) {
+  for (const price of timestampsToFetch) {
     allTimestamps.add(price.timestamp);
   }
 
-  let fetchedTimestamps = new Set();
+  const fetchedTimestamps = new Set();
   for (const price of fetchedPrices) {
     fetchedTimestamps.add(price.timestamp);
   }
@@ -45,7 +49,7 @@ const getHistoricalPrices = async () => {
     (x) => !fetchedTimestamps.has(x)
   ) as any;
 
-  let i = Math.floor(0.9 * pricesToFetch.length);
+  let priceIterator;
 
   let prices: PriceWithTimestamp[] = [];
 
@@ -56,22 +60,18 @@ const getHistoricalPrices = async () => {
     });
   }
 
-  console.log(pricesToFetch.length);
-
-  while (i / pricesToFetch.length < 0.98) {
-    let promises = range(BigInt(i), BigInt(i) + BigInt(100), BigInt(1)).map(
-      (index: any) => {
-        let fetcher = new UniswapV3FetcherHistorical(pricesToFetch[index]);
-        return [fetcher.fetchAll(["OHM"]), pricesToFetch[index]];
-      }
-    );
+  while (priceIterator < pricesToFetch.length - PROMISE_BATCH_SIZE + 1) {
+    let promises = [...Array(PROMISE_BATCH_SIZE).keys()].map((index: any) => {
+      let fetcher = new UniswapV3FetcherHistorical(pricesToFetch[index]);
+      return [fetcher.fetchAll(["OHM"]), pricesToFetch[index]];
+    });
     await Promise.all(promises)
       .then((results) => {
         results.forEach((result) => {
           if (result[0][0].value != null) {
             const price = {
               uniswapPrice: result[0][0].value,
-              timestamp: result[1],
+              timestamp: Number(result[1].toString()),
             };
             prices.push(price);
           }
@@ -80,24 +80,19 @@ const getHistoricalPrices = async () => {
       .catch(function (err) {
         console.error(err);
       });
-    i += 100;
-    console.log(i / pricesToFetch.length);
+    priceIterator += 100;
+    console.log(priceIterator / pricesToFetch.length);
   }
   return prices;
 };
 
-const writeResults = (results: any) => {
-  console.log("Saving results to file");
-  let json = JSON.stringify(results);
-  fs.writeFile(`uniswap-prices.json`, json, "utf8", () => {});
-};
 const readPrices = (file: string) => {
-  console.log("Reading price file");
   return JSON.parse(fs.readFileSync(file, "utf-8"));
 };
 
 async function main() {
-  const result = await getHistoricalPrices();
-  writeResults(result);
+  const prices = await getHistoricalPrices();
+  saveJSON(prices, UNISWAP_PRICES_FILEPATH);
 }
+
 main();
