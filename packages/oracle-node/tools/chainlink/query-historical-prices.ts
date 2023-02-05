2543533794067ab95782710444b6778d6b41c488
rucinpk <rucinpk@gmail.com>
2023-02-05 20:38:17 +0100

    fixed after review

--- packages/oracle-node/tools/chainlink/query-historical-prices.ts
+++ packages/oracle-node/tools/chainlink/query-historical-prices.ts
@@ -1,10 +1,12 @@
 import { contracts, abi } from "../../src/fetchers/chainlink/constants";
 import { Contract, ethers } from "ethers";
-import fs from "fs";
 import { saveJSON } from "../common/fs-utils";
 
 const AGGREGATOR_V3_INTERFACE_ABI = abi;
 const PRICE_FEED_ADDR = contracts["OHMv2"];
+const BATCH_SIZE = 100;
+const MAX_REVERTS_COUNT_TO_EXIT_PHASE = 10;
+const RESULT_FILE_PATH = "historical-chainlink-prices.json";
 
 const provider = new ethers.providers.JsonRpcProvider(
   "https://api.avax.network/ext/bc/C/rpc"
@@ -16,9 +18,8 @@ interface PriceWithTimestamp {
 }
 
 const getRoundId = async (priceFeedContract: Contract) => {
-  return BigInt(
-    (await priceFeedContract.latestRoundData()).roundId.toHexString()
-  );
+  const latestRoundData = await priceFeedContract.latestRoundData();
+  return BigInt(latestRoundData.roundId.toHexString());
 };
 
 const getPhaseId = (roundId: bigint) => {
@@ -49,55 +50,64 @@ const getAllPrices = async (priceFeed: Contract, latestPhaseId: bigint) => {
   return prices;
 };
 
+const fetchPricesFromBatch = async (contract: Contract, startingRoundId) => {
+  let roundId = startingRoundId;
+  let batchPrices: PriceWithTimestamp[] = [];
+  let numErrorsInBatch = 0;
+  let isPhaseReverted = false;
+
+  try {
+    const promises = [...Array(BATCH_SIZE).keys()].map((round: number) =>
+      contract.getRoundData(BigInt(round))
+    );
+
+    let isReverted = false;
+    await Promise.all(promises).then((results) => {
+      results.forEach((answer) => {
+        const price = BigInt(answer.answer);
+        if (price === 0n) {
+          isReverted = true;
+        } else {
+          const startedAtTimestamp = answer.startedAt;
+          roundId++;
+          batchPrices.push({
+            price: price.toString(),
+            timestamp: startedAtTimestamp.toString(),
+          });
+        }
+      });
+    });
+    if (isReverted) {
+      numErrorsInBatch++;
+    }
+  } catch (error) {
+    console.error(error);
+    numErrorsInBatch++;
+  }
+  if (numErrorsInBatch > MAX_REVERTS_COUNT_TO_EXIT_PHASE) {
+    isPhaseReverted = true;
+  }
+  return { batchPrices, isPhaseReverted };
+};
 const getAggregatorPrices = async (contract: Contract, phaseId: bigint) => {
   let prices: PriceWithTimestamp[] = [];
-
   let roundID = (phaseId << 64n) | BigInt("1");
 
-  let numErrors = 0;
   while (true) {
-    try {
-      const promises = [...Array(100).keys()].map((round: number) =>
-        contract.getRoundData(BigInt(round))
-      );
-
-      let isReverted = false;
-      await Promise.all(promises).then((results) => {
-        results.forEach((answer) => {
-          const price = BigInt(answer.answer);
-          if (price === 0n) {
-            isReverted = true;
-          } else {
-            const startedAtTimestamp = answer.startedAt;
-            roundID++;
-            prices.push({
-              price: price.toString(),
-              timestamp: startedAtTimestamp.toString(),
-            });
-            const date = new Date(
-              Number(prices[prices.length - 1].timestamp) * 1000
-            );
-          }
-        });
-      });
-      if (isReverted) {
-        if (numErrors > 10) {
-          break;
-        }
-        numErrors++;
-      }
-    } catch (error) {
-      console.error(error);
-      if (numErrors > 10) {
-        break;
-      }
-      numErrors++;
+    const { batchPrices, isPhaseReverted } = await fetchPricesFromBatch(
+      contract,
+      roundID
+    );
+    prices = [...prices, ...batchPrices];
+    if (isPhaseReverted) {
+      break;
     }
   }
+
   return prices;
 };
 
-async function query() {
+async function main() {
   const priceFeed = new Contract(
     PRICE_FEED_ADDR,
     AGGREGATOR_V3_INTERFACE_ABI,
@@ -106,12 +116,12 @@ async function query() {
   const roundId = await getRoundId(priceFeed);
   const phaseId = getPhaseId(roundId);
   const prices = await getAllPrices(priceFeed, phaseId);
-  saveJSON(prices, "historical-chainlink-prices.json");
+  saveJSON(prices, RESULT_FILE_PATH);
 }
 
 const runScript = async () => {
   try {
-    await query();
+    await main();
   } catch (error) {
     console.log(error);
   }
