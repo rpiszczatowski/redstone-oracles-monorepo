eaf4c633df047db613f400f9a87824071ad107c9
hatskier <suvorovalex1997@gmail.com>
2023-02-05 14:25:07 +0000

    fix: cvf-16 and cvf-17

--- packages/eth-contracts/contracts/RedstoneToken.sol
+++ packages/eth-contracts/contracts/RedstoneToken.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-uint constant MAX_SUPPLY = 50000000 * 1e18;
-
 /**
  * @title RedstoneToken
  * @dev Standard implementation of ERC20 for Redstone token
  */
 contract RedstoneToken is ERC20 {
+  uint256 constant MAX_SUPPLY = 50_000_000e18;
+
   address public minter;
 
   constructor(uint256 initialSupply) ERC20("Redstone", "REDSTONE") {
