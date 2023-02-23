43f5435ecd52fb444ed5395666aab9584e111df2
rucinpk <rucinpk@gmail.com>
2023-02-23 09:42:59 +0100

    added uniswap-v2/sushiswap rpc fetchers

--- packages/oracle-node/src/fetchers/sushiswap/SushiswapFetcher.ts
+++ packages/oracle-node/src/fetchers/sushiswap/SushiswapFetcher.ts
@@ -1,14 +1,28 @@
-import { DexFetcher } from "../DexFetcher";
+import { PricesObj } from "../../types";
+import { BaseFetcher } from "../BaseFetcher";
+import DexProxy from "./DexProxy";
 
-const symbolToPairIdObj: {
-  [symbol: string]: string;
-} = require("./sushiswap-symbol-to-pair-id.json");
+export class SushiswapFetcher extends BaseFetcher {
+  private dexProxy: DexProxy;
 
-const subgraphUrl =
-  "https://api.thegraph.com/subgraphs/name/sushiswap/exchange";
-
-export class SushiswapFetcher extends DexFetcher {
   constructor() {
-    super("sushiswap", subgraphUrl, symbolToPairIdObj);
+    super("sushiswap");
+    this.dexProxy = new DexProxy();
+  }
+
+  async fetchData(ids: string[]): Promise<any> {
+    return await this.dexProxy.getExchangeRates(ids);
+  }
+
+  async extractPrices(response: any): Promise<PricesObj> {
+    const pricesObj: PricesObj = {};
+
+    for (const id of Object.keys(response)) {
+      const decimalPrice =
+        Number(response[id].price) * Math.pow(10, -response[id].decimalPlaces);
+
+      pricesObj[id] = parseFloat(decimalPrice.toFixed(8));
+    }
+    return pricesObj;
   }
 }
