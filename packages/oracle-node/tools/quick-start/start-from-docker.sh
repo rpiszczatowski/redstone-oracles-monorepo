e7c502307b74d9b5beb68dbd0846b654f9f59ee3
hatskier <suvorovalex1997@gmail.com>
2023-03-09 10:35:30 +0000

    chore: docker tag updated in quick-start

--- packages/oracle-node/tools/quick-start/start-from-docker.sh
+++ packages/oracle-node/tools/quick-start/start-from-docker.sh
@@ -3,4 +3,4 @@
 set -x # to print commands as they are executed
 
 # Running oracle node
-docker run --env-file quick-start.env -it public.ecr.aws/y7v2w8b2/redstone-oracle-node:fef6d9b
+docker run --env-file quick-start.env -it public.ecr.aws/y7v2w8b2/redstone-oracle-node:6c5e4bf
