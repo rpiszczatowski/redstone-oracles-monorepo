28d0275c09f9edbe3832ba83bac0084f73347c1b
hatskier <suvorovalex1997@gmail.com>
2023-03-17 22:09:29 +0000

    chore: implemented script for updating sorted oracle address

--- packages/on-chain-relayer/src/scripts/mento/update-sorted-oracles-address.ts
+++ packages/on-chain-relayer/src/scripts/mento/update-sorted-oracles-address.ts
@@ -1,1 +1,18 @@
-console.log("TODO: implement");
+import { getAdapterContract } from "../../core/contract-interactions/get-contract";
+
+const NEW_SORTED_ORACLES_ADDRESS = "0x8536122Cff298dEEdD453A6Fb405953e83032935";
+
+(async () => {
+  const mentoAdapterContract = getAdapterContract();
+
+  console.log(
+    `Updating sorted oracle address to: ${NEW_SORTED_ORACLES_ADDRESS}`
+  );
+  const tx = await mentoAdapterContract.updateSortedOraclesAddress(
+    NEW_SORTED_ORACLES_ADDRESS
+  );
+
+  console.log(`Tx sent: ${tx.hash}`);
+  await tx.wait();
+  console.log(`Tx confirmed: ${tx.hash}`);
+})();
