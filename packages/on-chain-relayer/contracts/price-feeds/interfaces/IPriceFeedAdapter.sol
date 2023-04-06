bd9e99fab9807c50eb9c780e352ae2b67df3f192
hatskier <suvorovalex1997@gmail.com>
2023-04-06 03:26:04 +0100

    chore: license switched to busl

--- packages/on-chain-relayer/contracts/price-feeds/interfaces/IPriceFeedAdapter.sol
+++ packages/on-chain-relayer/contracts/price-feeds/interfaces/IPriceFeedAdapter.sol
@@ -1,23 +1,23 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 
 pragma solidity ^0.8.4;
 
 import "../../core/IPermissionlessPriceUpdater.sol";
 
 interface IPriceFeedAdapter is IPermissionlessPriceUpdater {
-  // Data feed ids management
   function getDataFeedsIds() external view returns (bytes32[] memory);
 
   function addDataFeedIdAndUpdateValues(bytes32 newDataFeedId, uint256 proposedTimestamp) external;
 
   function removeDataFeedId(bytes32 dataFeedId) external;
 
-  // Data feed values updating (requires attached redstone payload in calldata)
+  // Requires attached redstone payload in calldata
   function updateDataFeedsValues(uint256 proposedTimestamp) external;
 
-  // Reading values from the adapter contract storage
+  // Reads from adapter contract storage
   function getValueForDataFeed(bytes32 dataFeedId) external view returns (uint256);
 
+  // Reads from adapter contract storage
   function getValuesForDataFeeds(bytes32[] memory requestedDataFeedsIds)
     external
     view
--- packages/on-chain-relayer/contracts/price-feeds/with-rounds/PriceFeedWithRounds.sol
+++ packages/on-chain-relayer/contracts/price-feeds/with-rounds/PriceFeedWithRounds.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
+
 pragma solidity ^0.8.4;
 
 import "./PriceFeedsAdapterWithRounds.sol";
