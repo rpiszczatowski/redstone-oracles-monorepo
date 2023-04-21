4eb07993a8c738a36b8dac0ebc3565d6ffd87cbe
cehali <cezary.haliniarz@gmail.com>
2023-04-21 10:44:31 +0200

    chore: remove monitoring from monorepo

--- packages/oracle-monitoring/backend/src/shared/parse-array-to-object.ts
+++
@@ -1,5 +0,0 @@
-export const parseArrayToObject = (array: any[]) =>
-  array.reduce((accumulatedObject, currentItem) => {
-    const { _id, ...rest } = currentItem;
-    return { ...accumulatedObject, [_id]: rest };
-  }, {});
