71938ecfb40dfc76a79cc91c80308058d479f2b7
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2023-05-12 14:52:34 +0200

    feat: add sofrai token (#625)

--- packages/oracle-node/src/fetchers/newyorkfed/NewyorkfedFetcher.ts
+++ packages/oracle-node/src/fetchers/newyorkfed/NewyorkfedFetcher.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 import { PricesObj } from "../../types";
-import { stringifyError } from "../../utils/error-stringifier";
 import { BaseFetcher } from "../BaseFetcher";
+import dataFeedsFunctionNames from "./data-feeds-function-names.json";
 
 interface NewyorkfedResponse {
   refRates: NewyorkfedRefRate[];
@@ -9,8 +9,12 @@ interface NewyorkfedResponse {
 
 interface NewyorkfedRefRate {
   type: string;
-  percentRate: number;
+  percentRate?: number;
+  index?: number;
 }
+
+type NewyorkFedDataFeedsIds = keyof typeof dataFeedsFunctionNames;
+type NewyorkfedRefRateFunctionNames = "percentRate" | "index";
 
 const NEWYORKFED_RATES_URL =
   "https://markets.newyorkfed.org/api/rates/all/latest.json";
@@ -20,13 +24,13 @@ export class NewyorkfedFetcher extends BaseFetcher {
     super("newyorkfed");
   }
 
-  fetchData(_ids: string[]) {
+  fetchData(_ids: NewyorkFedDataFeedsIds[]) {
     return axios.get<NewyorkfedResponse>(NEWYORKFED_RATES_URL);
   }
 
   extractPrices(
     response: AxiosResponse<NewyorkfedResponse>,
-    ids: string[]
+    ids: NewyorkFedDataFeedsIds[]
   ): PricesObj {
     return this.extractPricesSafely(ids, (id) =>
       this.extractPricePair(response, id)
@@ -35,11 +39,13 @@ export class NewyorkfedFetcher extends BaseFetcher {
 
   private extractPricePair(
     response: AxiosResponse<NewyorkfedResponse>,
-    id: string
+    id: NewyorkFedDataFeedsIds
   ) {
     const rateFound = response.data.refRates.find((rate) => rate.type === id);
     if (rateFound) {
-      return { value: rateFound.percentRate, id };
+      const functionName = dataFeedsFunctionNames[id];
+      const value = rateFound[functionName as NewyorkfedRefRateFunctionNames];
+      return { value, id };
     } else {
       throw new Error(`Rate ${id} not found`);
     }
