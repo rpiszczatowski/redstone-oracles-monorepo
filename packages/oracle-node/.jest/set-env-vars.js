58683ce901aaad4b42ee26eb8bd4a24e28dbb6e3
cehali <cezary.haliniarz@gmail.com>
2023-05-12 13:23:35 +0200

    Fix main dry run test

--- packages/oracle-node/.jest/set-env-vars.js
+++ packages/oracle-node/.jest/set-env-vars.js
@@ -9,7 +9,6 @@ process.env.OVERRIDE_PRICE_CACHE_SERVICE_URLS = `["http://mock-price-cache-servi
 process.env.ETHERSCAN_API_URL = "http://mock-etherscan-api-url";
 process.env.ETHERSCAN_API_KEY = "test-api-key";
 process.env.LEVEL_DB_LOCATION = "level-db-for-tests";
-process.env.AVALANCHE_RPC_URL = "http://mock-avalanche-rpc-url";
 process.env.COINMARKETCAP_API_KEY = "coinmarketcap-api-key";
 process.env.MOCK_PRICES_URL_OR_PATH = "http://mock-fetcher";
 process.env.MIN_DATA_FEEDS_PERCENTAGE_FOR_BIG_PACKAGE = "50";
