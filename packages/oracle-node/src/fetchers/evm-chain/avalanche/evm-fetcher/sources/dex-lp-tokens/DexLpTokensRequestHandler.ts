07ef48b44114a94f7c72d14ecc577b450c3c7efd
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2023-05-12 15:33:04 +0200

    feat: add steak hut tokens (#623)

--- packages/oracle-node/src/fetchers/evm-chain/avalanche/evm-fetcher/sources/dex-lp-tokens/DexLpTokensRequestHandler.ts
+++ packages/oracle-node/src/fetchers/evm-chain/avalanche/evm-fetcher/sources/dex-lp-tokens/DexLpTokensRequestHandler.ts
@@ -2,20 +2,17 @@ import { Decimal } from "decimal.js";
 import { IEvmRequestHandlers } from "../../../../shared/IEvmRequestHandlers";
 import { buildMulticallRequests } from "../../../../shared/utils/build-multicall-request";
 import { extractValueFromMulticallResponse } from "../../../../shared/utils/extract-prices";
-import { getLastPrice } from "../../../../../../db/local-db";
 import { dexLpTokensContractsDetails } from ".";
 import { MulticallParsedResponses } from "../../../../../../types";
-import { TEN_AS_BASE_OF_POWER } from "../../../../shared/contants";
+import { getFairPriceForLpToken } from "../../../../shared/utils/get-fair-price-lp-token";
+import { serializeDecimalsForLpTokens } from "../../../../shared/utils/serialize-decimals-lp-tokens";
 
 // Fair LP Token Pricing has been implemented with the help of: https://blog.alphaventuredao.io/fair-lp-token-pricing/
 
 export type DexLpTokensDetailsKeys = keyof typeof dexLpTokensContractsDetails;
 
-type TokenReserve = Record<string, Decimal>;
-
-// We want to serialize all decimals to 18
-const STABLECOIN_DECIMALS_DIFF_TO_EIGHTEEN = 12;
-const BTC_DECIMALS_DIFF_TO_EIGHTEEN = 10;
+const FIRST_TOKEN_INDEXES_FROM_CONTRACT_RESPONSE = [0, 66];
+const SECOND_TOKEN_INDEXES_FROM_CONTRACT_RESPONSE = [66, 130];
 
 export class DexLpTokensRequestHandler implements IEvmRequestHandlers {
   prepareMulticallRequest(id: DexLpTokensDetailsKeys) {
@@ -38,89 +35,25 @@ export class DexLpTokensRequestHandler implements IEvmRequestHandlers {
       "getReserves"
     );
 
-    const firstTokenReserve = new Decimal(reserves.slice(0, 66));
+    const firstTokenReserve = new Decimal(
+      reserves.slice(...FIRST_TOKEN_INDEXES_FROM_CONTRACT_RESPONSE)
+    );
     const firstToken = tokensToFetch[0];
-    const secondTokenReserve = new Decimal(`0x${reserves.slice(66, 130)}`);
+    const secondTokenReserve = new Decimal(
+      `0x${reserves.slice(...SECOND_TOKEN_INDEXES_FROM_CONTRACT_RESPONSE)}`
+    );
     const secondToken = tokensToFetch[1];
     const tokenReserves = {
       [firstToken]: firstTokenReserve,
       [secondToken]: secondTokenReserve,
     };
 
-    const tokensReservesPrices =
-      this.getReservesTokensPricesFromDb(tokenReserves);
-    console.log({ tokensReservesPrices });
-    const reservesSerialized = this.serializeDecimals(tokenReserves);
-    console.log({ reservesSerialized });
+    const reservesSerialized = serializeDecimalsForLpTokens(tokenReserves);
 
-    if (
-      tokensReservesPrices &&
-      this.checkIfAllTokensFetched(tokensReservesPrices, tokenReserves)
-    ) {
-      const firstTokenReservePrice = tokensReservesPrices[firstToken];
-      const secondTokenReservePrice = tokensReservesPrices[secondToken];
-
-      const firstReserve = reservesSerialized[firstToken];
-      const secondReserve = reservesSerialized[secondToken];
-
-      const reservesMultiplied = firstReserve.mul(secondReserve);
-      const pricesMultiplied = firstTokenReservePrice.mul(
-        secondTokenReservePrice
-      );
-
-      const reservesMultipliedSqrt = reservesMultiplied.sqrt();
-      const pricesMultipliedSqrt = pricesMultiplied.sqrt();
-
-      const reservesPricesMultiplied =
-        reservesMultipliedSqrt.mul(pricesMultipliedSqrt);
-
-      const totalSupply = new Decimal(
-        extractValueFromMulticallResponse(response, address, "totalSupply")
-      );
-
-      return reservesPricesMultiplied.div(totalSupply).mul(2).toNumber();
-    }
-  }
-
-  getReservesTokensPricesFromDb(tokenReserves: TokenReserve) {
-    const tokenNames = Object.keys(tokenReserves);
-    const pricesObject: Record<string, Decimal> = {};
-    for (const token of tokenNames) {
-      const tokenPriceFromDb = getLastPrice(token);
-      if (tokenPriceFromDb) {
-        const price = tokenPriceFromDb.value;
-        pricesObject[token] = new Decimal(price);
-      }
-    }
-    return pricesObject;
-  }
-
-  serializeDecimals(tokenReserves: TokenReserve) {
-    const serializedTokenReserves = {} as TokenReserve;
-    for (const tokenName of Object.keys(tokenReserves)) {
-      let tokenReserveSerialized = tokenReserves[tokenName];
-      if (["USDC", "USDT"].includes(tokenName)) {
-        const multiplier = new Decimal(TEN_AS_BASE_OF_POWER).toPower(
-          STABLECOIN_DECIMALS_DIFF_TO_EIGHTEEN
-        );
-        tokenReserveSerialized = tokenReserves[tokenName].mul(multiplier);
-      } else if (tokenName === "BTC") {
-        const multiplier = new Decimal(TEN_AS_BASE_OF_POWER).toPower(
-          BTC_DECIMALS_DIFF_TO_EIGHTEEN
-        );
-        tokenReserveSerialized = tokenReserves[tokenName].mul(multiplier);
-      }
-      serializedTokenReserves[tokenName] = tokenReserveSerialized;
-    }
-    return serializedTokenReserves;
-  }
-
-  checkIfAllTokensFetched(
-    pricesObject: Record<string, Decimal>,
-    tokenReserves: TokenReserve
-  ) {
-    return (
-      Object.keys(pricesObject).length === Object.keys(tokenReserves).length
+    const totalSupply = new Decimal(
+      extractValueFromMulticallResponse(response, address, "totalSupply")
     );
+
+    return getFairPriceForLpToken(reservesSerialized, totalSupply);
   }
 }
