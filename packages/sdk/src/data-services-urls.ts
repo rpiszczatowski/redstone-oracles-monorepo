5f82d998c5fa419067194e6366c1de31c5f60aad
Michal Konopka <konopek68@gmail.com>
2023-06-06 18:08:16 +0200

    chore: remove data service url for redstone-primary-demo not deployed yet

--- packages/sdk/src/data-services-urls.ts
+++ packages/sdk/src/data-services-urls.ts
@@ -5,7 +5,7 @@ export const REDSTONE_DATA_SERVICES_URLS: Record<string, string[]> = {
     "https://oracle-gateway-1.a.redstone.finance",
     "https://oracle-gateway-2.a.redstone.finance",
   ],
-  "redstone-primary-demo": [DEMO_URL],
+  // TODO: NOT DEPLOYED YET "redstone-primary-demo": [],
   "redstone-avalanche-prod": [
     "https://oracle-gateway-1.a.redstone.finance",
     "https://oracle-gateway-2.a.redstone.finance",
