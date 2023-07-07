125ef9d549404f08c16c0a75939bb042948a134e
robal <robert@warp.cc>
2023-07-07 17:21:28 +0200

    chore: dry run checks improvements (#830)

--- packages/oracle-node/test/dry-run/dry-run-test.spec.ts
+++ packages/oracle-node/test/dry-run/dry-run-test.spec.ts
@@ -12,6 +12,7 @@ import { CronScheduler } from "../../src/schedulers/CronScheduler";
 import {
   clearPricesSublevel,
   closeLocalLevelDB,
+  getLastPrice,
   setupLocalDb,
 } from "../../src/db/local-db";
 import { getDryRunTestConfig } from "./dry-run-tests-configs";
@@ -67,24 +68,30 @@ describe("Main dry run test", () => {
   });
 
   test(`Dry run test for ${process.env.DRY_RUN_TEST_TYPE} manifest`, async () => {
-    /* 
+    /*
       We want to run Node 4 times because in order to calculate price of some tokens
-      we need price of another tokens e.g. 
+      we need price of another tokens e.g.
       USDC/USDT -> AVAX/ETH -> TJ_AVAX_ETH_LP -> YY_TJ_AVAX_ETH_LP
     */
     await runNodeMultipleTimes(config.manifest, config.nodeIterations);
-    const dataPackages = mockedBroadcaster.mock?.lastCall?.[0] ?? [];
-    const pricesForDataFeedId = getPricesForDataFeedId(dataPackages);
     const tokens = getTokensFromManifest(config.manifest);
     for (const token of tokens) {
-      const currentDataFeedPrice = pricesForDataFeedId[token];
+      const currentDataFeedPrice = getLastPrice(token);
       if (!currentDataFeedPrice) {
         console.log(`Missing token ${token} during dry run test`);
       }
       if (config.additionalCheck) {
-        config.additionalCheck(token, currentDataFeedPrice);
+        config.additionalCheck(token, currentDataFeedPrice?.value);
       }
     }
+    expect(mockedBroadcaster.mock.calls.length).toBeGreaterThan(0);
+    const dataPackages = mockedBroadcaster.mock.calls
+      .map((a) => a[0])
+      .reduce((p, c) => {
+        p.push(...c);
+        return p;
+      }, []);
+    const pricesForDataFeedId = getPricesForDataFeedId(dataPackages);
     const allTokensBroadcasted = Object.keys(pricesForDataFeedId).length;
     const requiredTokensCount =
       MIN_REQUIRED_MANIFEST_TOKENS_PERCENTAGE * tokens.length;
