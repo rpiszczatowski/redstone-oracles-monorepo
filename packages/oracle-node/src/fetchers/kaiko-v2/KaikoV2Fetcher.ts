0a45f95386d2809e20e0be69ffd7eb8ae805c402
Jakub Wojciechowski <jakub@alice.si>
2023-07-10 20:19:39 +0200

    feat: add Kaiko v2 fetcher (#834)
    * Fix FRXETH config
    
    * Kaiko V2 fetcher
    
    * Minor improvements
    
    ---------
    
    Co-authored-by: cehali <cezary.haliniarz@gmail.com>

---
+++ packages/oracle-node/src/fetchers/kaiko-v2/KaikoV2Fetcher.ts
@@ -0,0 +1,47 @@
+import {
+  MultiRequestFetcher,
+  RequestIdToResponse,
+} from "../MultiRequestFetcher";
+import { config } from "../../config";
+import axios from "axios";
+
+const KAIKO_PRICES_URL =
+  "https://eu.market-api.kaiko.io/v2/data/trades.v1/spot_exchange_rate";
+const KAIKO_REQUEST_HEADERS = {
+  "X-Api-Key": config.kaikoApiKey,
+  Accept: "application/json",
+};
+const KAIKO_REQUEST_PARAMS = {
+  page_size: 90,
+  interval: "10s",
+  sort: "desc",
+  extrapolate_missing_values: true,
+  startTime: new Date().getTime() - 1000 * 60 * 15,
+};
+
+export class KaikoV2Fetcher extends MultiRequestFetcher {
+  constructor() {
+    super("kaiko-v2");
+  }
+
+  buildKaikoApiUrl = (id: string): string => {
+    return `${KAIKO_PRICES_URL}/${id.toLowerCase()}/usd`;
+  };
+
+  override makeRequest(id: string): Promise<any> {
+    return axios.get(this.buildKaikoApiUrl(id), {
+      headers: KAIKO_REQUEST_HEADERS,
+      params: KAIKO_REQUEST_PARAMS,
+    });
+  }
+
+  override extractPrice(
+    dataFeedId: string,
+    responses: RequestIdToResponse
+  ): number | undefined {
+    if (responses[dataFeedId]) {
+      const price = responses[dataFeedId]?.data?.data[0].price;
+      return price ? Number(price) : undefined;
+    }
+  }
+}
