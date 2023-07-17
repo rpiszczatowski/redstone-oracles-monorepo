fd42e166db8c1135693453f09bdc70f7e61d20fc
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2023-07-17 17:32:05 +0200

    feat: update yahoo finance fetcher (#862)

--- packages/oracle-node/src/fetchers/yf-unofficial/YfUnofficialFetcher.ts
+++ packages/oracle-node/src/fetchers/yf-unofficial/YfUnofficialFetcher.ts
@@ -1,53 +1,28 @@
-import _ from "lodash";
-import { PricesObj } from "../../types";
+import yahooFinance from "yahoo-finance2";
+import { QuoteSummaryResult } from "yahoo-finance2/dist/esm/src/modules/quoteSummary-iface";
+import { MultiRequestFetcher } from "../MultiRequestFetcher";
 import { getRequiredPropValue } from "../../utils/objects";
-import { BaseFetcher } from "../BaseFetcher";
-import YahooFinanceProxy from "./YahooFinanceProxy";
 import symbolToId from "./yf-symbol-to-id.json";
 
-const idToSymbol = _.invert(symbolToId);
-
-export class YfUnofficialFetcher extends BaseFetcher {
-  private yahooFinanceProxy: YahooFinanceProxy;
+export class YfUnofficialFetcher extends MultiRequestFetcher {
   protected retryForInvalidResponse: boolean = true;
 
   constructor() {
     super("yf-unofficial");
-    this.yahooFinanceProxy = new YahooFinanceProxy();
   }
 
-  override convertIdToSymbol(id: string) {
-    return getRequiredPropValue(idToSymbol, id);
+  override makeRequest(dataFeedId: string): Promise<QuoteSummaryResult> {
+    const symbol = getRequiredPropValue(symbolToId, dataFeedId);
+    return yahooFinance.quoteSummary(symbol, {
+      modules: ["price"],
+    });
   }
 
-  override convertSymbolToId(symbol: string) {
-    return getRequiredPropValue(symbolToId, symbol);
-  }
-
-  async fetchData(ids: string[]) {
-    return await this.yahooFinanceProxy.getExchangeRates(ids);
-  }
-
-  extractPrices(response: any): PricesObj {
-    return this.extractPricesSafely(Object.keys(response), (symbol) =>
-      this.extractPricePair(response, symbol)
-    );
-  }
-
-  private extractPricePair(response: any, symbol: string) {
-    const details = response[symbol];
-    let value: any = details.price.regularMarketPrice;
-
-    if (isNaN(value)) {
-      if (!!value && value.raw) {
-        value = value.raw;
-      } else {
-        this.logger.warn(
-          `Empty regular market price: ${JSON.stringify(details.price)}`
-        );
-      }
-    }
-
-    return { id: symbol, value };
+  override extractPrice(
+    dataFeedId: string,
+    responses: { [datafeedId: string]: QuoteSummaryResult }
+  ): number | undefined {
+    const details = responses[dataFeedId];
+    return details?.price?.regularMarketPrice;
   }
 }
