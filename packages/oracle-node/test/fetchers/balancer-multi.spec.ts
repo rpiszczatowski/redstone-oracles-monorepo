fd0ff4f776b15993203e5ad1cc46f3ffbe1e10fa
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2023-07-21 17:03:48 +0200

    chore: update swell balancer pools (#872)

--- packages/oracle-node/test/fetchers/balancer-multi.spec.ts
+++ packages/oracle-node/test/fetchers/balancer-multi.spec.ts
@@ -1,10 +1,12 @@
-import {
-  clearPricesSublevel,
-  closeLocalLevelDB,
-  setupLocalDb,
-} from "../../src/db/local-db";
-import fetchers from "../../src/fetchers/index";
+import { MockProvider, deployMockContract } from "ethereum-waffle";
+import { closeLocalLevelDB, setupLocalDb } from "../../src/db/local-db";
 import { saveMockPriceInLocalDb } from "./_helpers";
+import BalancerVaultAbi from "../../src/fetchers/balancer-multi/BalancerVault.abi.json";
+import {
+  BALANCER_VAULT_ADDRESS,
+  BalancerMultiFetcher,
+} from "../../src/fetchers/balancer-multi/BalancerMultiFetcher";
+import { balancerMultiConfigs } from "../../src/fetchers/balancer-multi/balancer-multi-configs";
 
 const mockDeltasResponse = {
   "0xf951e335afb289353dc249e82926178eac7ded78": "1000000000000000",
@@ -12,19 +14,21 @@ const mockDeltasResponse = {
   "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2": "-1025056002749498",
 };
 
-jest.mock("@balancer-labs/sdk", () => ({
-  __esModule: true,
-  ...jest.requireActual("@balancer-labs/sdk"),
-  BalancerSDK: jest.fn().mockImplementation(() => ({
-    swaps: {
-      queryExactIn: () => mockDeltasResponse,
-    },
-  })),
-}));
-
 describe("balancer multi fetcher", () => {
-  beforeAll(() => {
+  let provider: MockProvider;
+
+  beforeAll(async () => {
     setupLocalDb();
+
+    provider = new MockProvider();
+    const [wallet] = provider.getWallets();
+    const contract = await deployMockContract(wallet, BalancerVaultAbi);
+    await contract.mock.queryBatchSwap.returns([
+      "1000000000000000",
+      "0",
+      "-1025056002749498",
+    ]);
+    (BALANCER_VAULT_ADDRESS as any) = contract.address;
   });
 
   afterAll(async () => {
@@ -34,9 +38,14 @@ describe("balancer multi fetcher", () => {
   test("should properly fetch data", async () => {
     await saveMockPriceInLocalDb(1635.27, "ETH");
 
-    const sut = fetchers["balancer-multi-sweth"];
+    const fetcher = new BalancerMultiFetcher(
+      "balancer-multi-weth-test",
+      balancerMultiConfigs.WETH,
+      "WETH",
+      provider
+    );
 
-    const result = await sut.fetchAll(["SWETH"]);
+    const result = await fetcher.fetchAll(["SWETH"]);
     expect(result).toEqual([
       {
         symbol: "SWETH",
