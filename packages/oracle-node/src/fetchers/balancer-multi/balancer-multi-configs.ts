02133db9f46333eab1310b1570b63e56a2668772
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2023-08-04 12:50:31 +0200

    feat: add balancer pools for GHO (#901)

--- packages/oracle-node/src/fetchers/balancer-multi/balancer-multi-configs.ts
+++ packages/oracle-node/src/fetchers/balancer-multi/balancer-multi-configs.ts
@@ -38,9 +38,6 @@ export const balancerMultiConfigs = {
         "0x60d604890feaa0b5460b28a424407c24fe89374a",
         "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
       ],
-
-      tokenInForSwaps: "0xf951e335afb289353dc249e82926178eac7ded78",
-      tokenOutFromSwaps: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
     },
   },
   WSTETH: {
@@ -63,8 +60,6 @@ export const balancerMultiConfigs = {
         "0xf951e335afb289353dc249e82926178eac7ded78",
         "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
       ],
-      tokenInForSwaps: "0xf951e335afb289353dc249e82926178eac7ded78",
-      tokenOutFromSwaps: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
     },
   },
   DAI: {
@@ -105,9 +100,88 @@ export const balancerMultiConfigs = {
         "0xfa24a90a3f2bbe5feea92b95cd0d14ce709649f9",
         "0x6b175474e89094c44da98b954eedeac495271d0f",
       ],
-      marketSp: "1.019442252013350645857275711595224196226780743325017064",
-      tokenInForSwaps: "0x40d16fc0246ad3160ccc09b8d0d3a2cd28ae6c2f",
-      tokenOutFromSwaps: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    },
+  },
+  USDC: {
+    GHO: {
+      tokenIn: "0x40d16fc0246ad3160ccc09b8d0d3a2cd28ae6c2f", // GHO
+      tokenOut: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
+      swapAmount: BIGGER_AMOUNT,
+      swapAmountForSwaps: BIGGER_AMOUNT,
+      priceMultiplier: 10e11,
+      swaps: [
+        {
+          poolId:
+            "0xc2b021133d1b0cf07dba696fd5dd89338428225b000000000000000000000598",
+          assetInIndex: 0,
+          assetOutIndex: 1,
+          amount: BIGGER_AMOUNT.toString(),
+          userData: "0x",
+        },
+        {
+          poolId:
+            "0xc443c15033fcb6cf72cc24f1bda0db070ddd9786000000000000000000000593",
+          assetInIndex: 1,
+          assetOutIndex: 2,
+          amount: "0",
+          userData: "0x",
+        },
+        {
+          poolId:
+            "0xc50d4347209f285247bda8a09fc1c12ce42031c3000000000000000000000590",
+          assetInIndex: 2,
+          assetOutIndex: 3,
+          amount: "0",
+          userData: "0x",
+        },
+      ],
+      tokenAddresses: [
+        "0x40d16fc0246ad3160ccc09b8d0d3a2cd28ae6c2f",
+        "0xc443c15033fcb6cf72cc24f1bda0db070ddd9786",
+        "0xc50d4347209f285247bda8a09fc1c12ce42031c3",
+        "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+      ],
+    },
+  },
+  USDT: {
+    GHO: {
+      tokenIn: "0x40d16fc0246ad3160ccc09b8d0d3a2cd28ae6c2f", // GHO
+      tokenOut: "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
+      swapAmount: BIGGER_AMOUNT,
+      swapAmountForSwaps: BIGGER_AMOUNT,
+      priceMultiplier: 10e11,
+      swaps: [
+        {
+          poolId:
+            "0xc2b021133d1b0cf07dba696fd5dd89338428225b000000000000000000000598",
+          assetInIndex: 0,
+          assetOutIndex: 1,
+          amount: BIGGER_AMOUNT.toString(),
+          userData: "0x",
+        },
+        {
+          poolId:
+            "0xc443c15033fcb6cf72cc24f1bda0db070ddd9786000000000000000000000593",
+          assetInIndex: 1,
+          assetOutIndex: 2,
+          amount: "0",
+          userData: "0x",
+        },
+        {
+          poolId:
+            "0xcfae6e251369467f465f13836ac8135bd42f8a56000000000000000000000591",
+          assetInIndex: 2,
+          assetOutIndex: 3,
+          amount: "0",
+          userData: "0x",
+        },
+      ],
+      tokenAddresses: [
+        "0x40d16fc0246ad3160ccc09b8d0d3a2cd28ae6c2f",
+        "0xc443c15033fcb6cf72cc24f1bda0db070ddd9786",
+        "0xcfae6e251369467f465f13836ac8135bd42f8a56",
+        "0xdac17f958d2ee523a2206206994597c13d831ec7",
+      ],
     },
   },
 };
