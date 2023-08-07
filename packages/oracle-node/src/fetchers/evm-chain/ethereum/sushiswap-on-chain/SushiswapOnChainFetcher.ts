a4f9efef887251c1c67b01e87b3fb93cc1711522
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2023-08-07 15:44:21 +0200

    feat: add PREMIA token (#930)

--- packages/oracle-node/src/fetchers/evm-chain/ethereum/sushiswap-on-chain/SushiswapOnChainFetcher.ts
+++ packages/oracle-node/src/fetchers/evm-chain/ethereum/sushiswap-on-chain/SushiswapOnChainFetcher.ts
@@ -1,11 +1,15 @@
+import { providers } from "ethers";
 import {
   PoolsConfig,
   UniswapV2LikeFetcher,
 } from "../../../uniswap-v2-like/UniswapV2LikeFetcher";
-import { ethereumProvider } from "../../../../utils/blockchain-providers";
 
 export class SushiswapOnChainFetcher extends UniswapV2LikeFetcher {
-  constructor(name: string, protected readonly poolsConfig: PoolsConfig) {
-    super(name, poolsConfig, ethereumProvider);
+  constructor(
+    name: string,
+    protected readonly poolsConfig: PoolsConfig,
+    protected readonly provider: providers.Provider
+  ) {
+    super(name, poolsConfig, provider);
   }
 }
