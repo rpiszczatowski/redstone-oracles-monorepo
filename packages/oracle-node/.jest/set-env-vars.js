79ff838fa4c10ffe2b60a84e9117da9f1c5f9d36
Mike <konopek68@gmail.com>
2023-08-07 10:28:05 +0200

    feat: add fallback provider to on-chain-relayer (#908)
    * feat: add fallback provider to on-chain-relayer
    
    * fix: update RPC_URL to RPC_URLS

--- packages/oracle-node/.jest/set-env-vars.js
+++ packages/oracle-node/.jest/set-env-vars.js
@@ -12,5 +12,5 @@ process.env.COINMARKETCAP_API_KEY = "coinmarketcap-api-key";
 process.env.MOCK_PRICES_URL_OR_PATH = "http://mock-fetcher";
 process.env.MIN_DATA_FEEDS_PERCENTAGE_FOR_BIG_PACKAGE = "50";
 if (!process.env.DRY_RUN_TEST_TYPE) {
-  process.env.AVALANCHE_RPC_URL = "http://mock-avalanche-rpc-url";
+  process.env.AVALANCHE_RPC_URLS = '["http://mock-avalanche-rpc-url"]';
 }
