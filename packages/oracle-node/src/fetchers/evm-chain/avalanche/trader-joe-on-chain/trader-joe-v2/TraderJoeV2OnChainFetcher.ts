82172983f2de84aa581e662bb87c3d5f301fb18e
robal <robert@warp.cc>
2023-08-14 13:57:06 +0200

    feat: add slippage calculation to TraderJoeV2OnChainFetcher (#944)

--- packages/oracle-node/src/fetchers/evm-chain/avalanche/trader-joe-on-chain/trader-joe-v2/TraderJoeV2OnChainFetcher.ts
+++ packages/oracle-node/src/fetchers/evm-chain/avalanche/trader-joe-on-chain/trader-joe-v2/TraderJoeV2OnChainFetcher.ts
@@ -1,76 +1,336 @@
 import Decimal from "decimal.js";
-import { Contract, providers } from "ethers";
+import {
+  ContractCallContext,
+  ContractCallResults,
+  Multicall,
+} from "ethereum-multicall";
+import { BigNumber, ethers, providers } from "ethers";
+import { SlippageData } from "redstone-utils/src/types";
+import { getLastPrice, getRawPriceOrFail } from "../../../../../db/local-db";
 import { DexOnChainFetcher } from "../../../../dex-on-chain/DexOnChainFetcher";
-import abi from "./TraderJoeV2LBPair.abi.json";
+import pairAbi from "./TraderJoeV2LBPair.abi.json";
+import routerAbi from "./TraderJoeV2LBRouter.abi.json";
 
 // Price calculation based on https://docs.traderjoexyz.com/guides/price-from-id
-
 const ONE_AS_DECIMAL = new Decimal(1);
 const BIN_STEP_DIVIDER = 10000;
 const BIN_ID_DEFAULT_SUBTRACTION = 8388608;
+const SLIPPAGE_SIMULATION_AMOUT = 10_000;
+const HUNDERD_PERCENT = 100;
 
-interface BinConfig {
-  binId: Decimal;
-  binStep: Decimal;
+interface MulticallResult {
+  spotPrice: Decimal;
+  buySlippage?: Decimal;
+  sellSlippage?: Decimal;
+}
+
+interface TokenConfig {
+  symbol: string;
+  decimals: number;
 }
 
 interface PairConfig {
-  [symbol: string]: {
-    address: string;
-    symbolX: string;
-    symbolY: string;
-  };
+  routerAddress: string;
+  pairAddress: string;
+  symbolX: string;
+  symbolXDecimals: number;
+  symbolY: string;
+  symbolYDecimals: number;
 }
 
-export class TraderJoeV2OnChainFetcher extends DexOnChainFetcher<BinConfig> {
+interface PairsConfig {
+  [symbol: string]: PairConfig;
+}
+
+export class TraderJoeV2OnChainFetcher extends DexOnChainFetcher<MulticallResult> {
   protected retryForInvalidResponse: boolean = true;
 
   constructor(
     name: string,
-    private readonly pairConfig: PairConfig,
+    private readonly pairsConfig: PairsConfig,
     private readonly provider: providers.Provider
   ) {
     super(name);
   }
 
-  override async makeRequest(dataFeedId: string): Promise<BinConfig> {
-    const pairConfigForDataFeed = this.pairConfig[dataFeedId];
-    if (!pairConfigForDataFeed) {
-      throw new Error(
-        `Missing pair config for ${dataFeedId} in ${this.name} fetcher`
+  multicallAddress: undefined | string = undefined;
+  public overrideMulticallAddress(address: string) {
+    this.multicallAddress = address;
+  }
+
+  override async makeRequest(assetId: string): Promise<MulticallResult> {
+    const baseTokenConfig = this.getBaseTokenConfig(assetId);
+    const pairedTokenConfig = this.getPairedTokenConfig(assetId);
+    const baseTokenPrice = getLastPrice(assetId)?.value;
+    const pairedTokenPrice = Number(
+      getRawPriceOrFail(pairedTokenConfig.symbol).value
+    );
+    let swapAmountBase: Decimal | undefined;
+    const swapAmountPaired = this.convertDollarsToSubTokens(
+      SLIPPAGE_SIMULATION_AMOUT,
+      pairedTokenPrice,
+      pairedTokenConfig.decimals
+    );
+    if (baseTokenPrice) {
+      swapAmountBase = this.convertDollarsToSubTokens(
+        SLIPPAGE_SIMULATION_AMOUT,
+        baseTokenPrice,
+        baseTokenConfig.decimals
       );
     }
-
-    const lbPair = new Contract(
-      pairConfigForDataFeed.address,
-      abi,
-      this.provider
+    const callContexts = this.buildContractCallContext(
+      assetId,
+      swapAmountBase,
+      swapAmountPaired
     );
 
-    const binId = await lbPair.getActiveId();
-    const binStep = await lbPair.getBinStep();
+    const multicallResult: ContractCallResults =
+      await this.createMulticallInstance().call(callContexts);
 
-    return {
-      binStep: new Decimal(binStep),
-      binId: new Decimal(binId),
-    };
+    const spotPrice = this.getSpotPriceFromMulticallResult(
+      multicallResult,
+      pairedTokenPrice
+    );
+    let buySlippage: Decimal | undefined;
+    let sellSlippage: Decimal | undefined;
+    if (baseTokenPrice) {
+      const basePriceInPaired = baseTokenPrice / pairedTokenPrice;
+      const pairedPriceInBase = pairedTokenPrice / baseTokenPrice;
+      buySlippage = this.getSlippageInPercent(
+        swapAmountPaired,
+        this.getTokensOutFromMulticallResult(multicallResult, "slippage_buy"),
+        pairedTokenConfig.decimals,
+        baseTokenConfig.decimals,
+        basePriceInPaired
+      );
+      sellSlippage = this.getSlippageInPercent(
+        swapAmountBase!,
+        this.getTokensOutFromMulticallResult(multicallResult, "slippage_sell"),
+        baseTokenConfig.decimals,
+        pairedTokenConfig.decimals,
+        pairedPriceInBase
+      );
+    }
+    return { spotPrice, buySlippage, sellSlippage };
   }
 
   override calculateSpotPrice(
-    _dataFeedId: string,
-    response: BinConfig
-  ): number {
-    const { binId, binStep } = response;
-    const binStepDivided = binStep.div(BIN_STEP_DIVIDER);
-    const binStepPlusOne = ONE_AS_DECIMAL.add(binStepDivided);
-    const binIdSerialized = binId.minus(BIN_ID_DEFAULT_SUBTRACTION);
-
-    return binStepPlusOne.pow(binIdSerialized).toNumber();
+    _assetId: string,
+    response: MulticallResult
+  ): string {
+    return response.spotPrice.toString();
   }
 
-  override calculateLiquidity(): number {
-    throw new Error(
-      `calculateLiquidity is not implemented for ${this.getName()}`
+  override calculateLiquidity(
+    _assetId: string,
+    _response: MulticallResult
+  ): undefined {
+    // no clear definition of liquidity in trader-joe v2
+    return undefined;
+  }
+
+  override calculateSlippage(
+    _assetId: string,
+    response: MulticallResult
+  ): SlippageData[] {
+    if (!response.buySlippage || !response.sellSlippage) {
+      return [];
+    }
+    return [
+      {
+        direction: "buy",
+        simulationValueInUsd: String(SLIPPAGE_SIMULATION_AMOUT),
+        slippageAsPercent: response.buySlippage.toString(),
+      },
+      {
+        direction: "sell",
+        simulationValueInUsd: String(SLIPPAGE_SIMULATION_AMOUT),
+        slippageAsPercent: response.sellSlippage.toString(),
+      },
+    ];
+  }
+
+  private getTokensOutFromMulticallResult(
+    multicallResult: ContractCallResults,
+    callReference: string
+  ): Decimal {
+    return this.decimal(
+      multicallResult.results.routerContract?.callsReturnContext.find(
+        (result) => result.reference === callReference
+      )!.returnValues[1]
     );
+  }
+
+  private getSpotPriceFromMulticallResult(
+    multicallResult: ContractCallResults,
+    pairedTokenPrice: number
+  ) {
+    const poolResults = multicallResult.results.poolContract.callsReturnContext;
+    const binStep = this.decimal(
+      poolResults.find((result) => result.reference === "getBinStep")!
+        .returnValues[0]
+    );
+    const binId = this.decimal(
+      poolResults.find((result) => result.reference === "getActiveId")!
+        .returnValues[0]
+    );
+    const binStepDivided = binStep.div(BIN_STEP_DIVIDER);
+    const binStepPlusOne = ONE_AS_DECIMAL.add(binStepDivided);
+    const binIdSerialized = binId.sub(BIN_ID_DEFAULT_SUBTRACTION);
+
+    const baseInPairedPrice = binStepPlusOne.pow(binIdSerialized);
+    const basePriceInDollars = baseInPairedPrice.mul(pairedTokenPrice);
+    return basePriceInDollars;
+  }
+
+  private convertSubTokensToTokens(amount: Decimal, decimals: number): Decimal {
+    return amount.div(this.decimal(10).toPower(decimals));
+  }
+
+  private getSlippageInPercent(
+    subtokensIn: Decimal,
+    subtokensOut: Decimal,
+    tokensInDecimals: number,
+    tokensOutDecimals: number,
+    spotPrice: number
+  ): Decimal {
+    const tokensSent = this.convertSubTokensToTokens(
+      subtokensIn,
+      tokensInDecimals
+    );
+    const tokensGot = this.convertSubTokensToTokens(
+      subtokensOut,
+      tokensOutDecimals
+    );
+    const actualPrice = tokensSent.div(tokensGot);
+    return actualPrice
+      .sub(spotPrice)
+      .abs()
+      .div(actualPrice)
+      .mul(HUNDERD_PERCENT);
+  }
+
+  private decimal(bigNumberLike: {}): Decimal {
+    return new Decimal(BigNumber.from(bigNumberLike).toString());
+  }
+
+  private isXBase(assetId: string) {
+    return this.pairsConfig[assetId].symbolX == assetId;
+  }
+
+  private getTokenConfig(assetId: string, isX: boolean) {
+    return isX
+      ? {
+          symbol: this.pairsConfig[assetId].symbolX,
+          decimals: this.pairsConfig[assetId].symbolXDecimals,
+        }
+      : {
+          symbol: this.pairsConfig[assetId].symbolY,
+          decimals: this.pairsConfig[assetId].symbolYDecimals,
+        };
+  }
+
+  private getBaseTokenConfig(assetId: string): TokenConfig {
+    return this.getTokenConfig(assetId, this.isXBase(assetId));
+  }
+
+  private getPairedTokenConfig(assetId: string) {
+    return this.getTokenConfig(assetId, !this.isXBase(assetId));
+  }
+
+  private convertDollarsToSubTokens(
+    amountInDolars: number,
+    tokenPrice: number,
+    tokenDecimals: number
+  ): Decimal {
+    return this.decimal(
+      ethers.utils
+        .parseUnits(
+          (amountInDolars / tokenPrice).toFixed(tokenDecimals),
+          tokenDecimals
+        )
+        .toString()
+    );
+  }
+
+  private createMulticallInstance() {
+    return new Multicall({
+      ethersProvider: this.provider,
+      tryAggregate: false, // throw on error
+      multicallCustomContractAddress: this.multicallAddress,
+    });
+  }
+
+  private buildContractCallContext(
+    assetId: string,
+    swapAmountBase: Decimal | undefined,
+    swapAmountPaired: Decimal
+  ): ContractCallContext[] {
+    const poolAddress = this.pairsConfig[assetId].pairAddress;
+
+    const calls: ContractCallContext[] = [this.preparePoolCall(poolAddress)];
+    if (swapAmountBase) {
+      const routerCall = this.prepareRouterCall(
+        poolAddress,
+        assetId,
+        swapAmountPaired,
+        swapAmountBase
+      );
+      calls.push(routerCall);
+    }
+    return calls;
+  }
+
+  private preparePoolCall(poolAddress: string) {
+    return {
+      reference: "poolContract",
+      contractAddress: poolAddress,
+      abi: pairAbi,
+      calls: [
+        {
+          reference: "getActiveId",
+          methodName: "getActiveId",
+          methodParameters: [],
+        },
+        {
+          reference: "getBinStep",
+          methodName: "getBinStep",
+          methodParameters: [],
+        },
+      ],
+    };
+  }
+
+  private prepareRouterCall(
+    poolAddress: string,
+    assetId: string,
+    swapAmountPaired: Decimal,
+    swapAmountBase: Decimal
+  ) {
+    return {
+      reference: "routerContract",
+      contractAddress: this.pairsConfig[assetId].routerAddress,
+      abi: routerAbi.abi,
+      calls: [
+        {
+          reference: "slippage_buy",
+          methodName: "getSwapOut",
+          methodParameters: [
+            poolAddress,
+            swapAmountPaired.toString(),
+            !this.isXBase(assetId),
+          ],
+        },
+        {
+          reference: "slippage_sell",
+          methodName: "getSwapOut",
+          methodParameters: [
+            poolAddress,
+            swapAmountBase.toString(),
+            this.isXBase(assetId),
+          ],
+        },
+      ],
+    };
   }
 }
--- packages/oracle-node/test/fetchers/trader-joe-v2.spec.ts
+++ packages/oracle-node/test/fetchers/trader-joe-v2.spec.ts
@@ -2,36 +2,125 @@ import {
   MockContract,
   MockProvider,
   deployMockContract,
+  deployContract,
 } from "ethereum-waffle";
 import { TraderJoeV2OnChainFetcher } from "../../src/fetchers/evm-chain/avalanche/trader-joe-on-chain/trader-joe-v2/TraderJoeV2OnChainFetcher";
-import abi from "../../src/fetchers/evm-chain/avalanche/trader-joe-on-chain/trader-joe-v2/TraderJoeV2LBPair.abi.json";
+import pairAbi from "../../src/fetchers/evm-chain/avalanche/trader-joe-on-chain/trader-joe-v2/TraderJoeV2LBPair.abi.json";
+import routerAbi from "../../src/fetchers/evm-chain/avalanche/trader-joe-on-chain/trader-joe-v2/TraderJoeV2LBRouter.abi.json";
 import configs from "../../src/fetchers/evm-chain/avalanche/trader-joe-on-chain/trader-joe-v2/trader-joe-v2-on-chain-fetchers-configs.json";
+import {
+  clearPricesSublevel,
+  closeLocalLevelDB,
+  setupLocalDb,
+} from "../../src/db/local-db";
+import multicall3Json from "../abis/Multicall3.deployment.json";
+import { Contract } from "ethers";
+import { saveMockPriceInLocalDb } from "./_helpers";
+
+const FETCHER_TO_TEST = "trader-joe-v2-avalanche-on-chain-usdc";
+const TOKEN_TO_TEST = "EUROC";
+const PAIRED_TOKEN = "USDC"
 
 describe("Trader Joe - EUROC token", () => {
-  let traderJoeV2Contract: MockContract;
+  let pairContract: MockContract;
+  let routerContract: MockContract;
   let provider: MockProvider;
+  let multicall: Contract;
 
   beforeAll(async () => {
+    setupLocalDb();
     provider = new MockProvider();
     const [wallet] = provider.getWallets();
 
-    traderJoeV2Contract = await deployMockContract(wallet, abi);
-    await traderJoeV2Contract.mock.getActiveId.returns(8388754);
-    await traderJoeV2Contract.mock.getBinStep.returns(5);
+    pairContract = await deployMockContract(wallet, pairAbi, {
+      address: configs[FETCHER_TO_TEST][TOKEN_TO_TEST].pairAddress,
+    });
+    routerContract = await deployMockContract(wallet, routerAbi.abi, {
+      address: configs[FETCHER_TO_TEST][TOKEN_TO_TEST].routerAddress,
+    });
+
+    multicall = await deployContract(wallet, multicall3Json);
+  });
+
+  beforeEach(async () => {
+    await clearPricesSublevel();
+  });
+
+  afterAll(async () => {
+    await closeLocalLevelDB();
+  });
+
+  // despite of the supposedly synchronous interface waffle mock setup has to be awaited, quiet the compiler
+  const asAwaitable = <T = void>(awaitableObject: any): Promise<T> => {
+    return awaitableObject as unknown as Promise<T>;
+  };
+
+  test("Should properly fetch data, no slippage", async () => {
+    await asAwaitable(pairContract.mock.getActiveId.returns("8388754"));
+    await asAwaitable(pairContract.mock.getBinStep.returns("5"));
+    // do price for TOKEN_TO_TEST available, so no slippage returned
+    await saveMockPriceInLocalDb(1, PAIRED_TOKEN);
+
+    const fetcher = new TraderJoeV2OnChainFetcher(
+      "trader-joe-v2-test-fetcher",
+      configs[FETCHER_TO_TEST],
+      provider
+    );
+    fetcher.overrideMulticallAddress(multicall.address);
+
+    const result = await fetcher.fetchAll([TOKEN_TO_TEST]);
+
+    expect(result).toEqual([
+      {
+        symbol: TOKEN_TO_TEST,
+        value: "1.075710911553001485",
+        metadata: {
+          slippage: []
+        },
+      },
+    ]);
   });
 
   test("Should properly fetch data", async () => {
-    const config = {
-      ...configs["trader-joe-v2-on-chain-usdc"].EUROC,
-      address: traderJoeV2Contract.address,
-    };
+    await asAwaitable(pairContract.mock.getActiveId.returns("8388754"));
+    await asAwaitable(pairContract.mock.getBinStep.returns("5"));
+    await asAwaitable(
+      routerContract.mock.getSwapOut
+        .returns("0", "20000000000", "0") // buy
+        .returns("0", "5000000000", "0")
+    );
+    await saveMockPriceInLocalDb(1, PAIRED_TOKEN);
+    await saveMockPriceInLocalDb(1, TOKEN_TO_TEST);
+
     const fetcher = new TraderJoeV2OnChainFetcher(
       "trader-joe-v2-test-fetcher",
-      { EUROC: config },
+      configs[FETCHER_TO_TEST],
       provider
     );
+    fetcher.overrideMulticallAddress(multicall.address);
 
-    const result = await fetcher.fetchAll(["EUROC"]);
-    expect(result).toEqual([{ symbol: "EUROC", value: 1.0757109115530015 }]);
+    const result = await fetcher.fetchAll([TOKEN_TO_TEST]);
+
+    expect(result).toEqual([
+      {
+        symbol: TOKEN_TO_TEST,
+        value: "1.075710911553001485",
+        metadata: {
+          slippage: [
+            {
+              direction: "buy",
+              simulationValueInUsd: "10000",
+              slippageAsPercent: "100",
+            },
+            {
+              direction: "sell",
+              simulationValueInUsd: "10000",
+              slippageAsPercent: "50",
+            },
+          ],
+        },
+      },
+    ]);
   });
 });
+
