a174315a6cc2931d36fa97cd5a2bbff665f1343c
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2023-08-29 16:36:58 +0200

    feat: add YieldYak LP tokens to arbitrum (#1013)

--- packages/oracle-node/src/fetchers/evm-chain/avalanche/evm-fetcher/sources/yield-yak/yieldYakTokensContractsDetails.ts
+++ packages/oracle-node/src/fetchers/evm-chain/avalanche/evm-fetcher/sources/yield-yak/yieldYakTokensContractsDetails.ts
@@ -1,53 +1,49 @@
-import YieldYakAaveAVAXAbi from "./YYAaveAVAX.abi.json";
-import YYPangolinSAVAXAbi from "./YYPangolinSAVAX.abi.json";
-import YYTraderJoeLPTokenAbi from "./YYTraderJoeLPToken.abi.json";
-import YYPangolinLPTokenAbi from "./YYPangolinLPToken.abi.json";
-import YYGLPTokenAbi from "./YYGLP.abi.json";
+import YieldYakLpTokenAbi from "../../../../shared/abis/YieldYakLPToken.abi.json";
 
 export const yieldYakTokensContractsDetails = {
   YY_AAVE_AVAX: {
     address: "0xaAc0F2d0630d1D09ab2B5A400412a4840B866d95",
-    abi: YieldYakAaveAVAXAbi,
+    abi: YieldYakLpTokenAbi,
     tokenToFetch: "AVAX",
   },
   YYAV3SA1: {
     address: "0xaAc0F2d0630d1D09ab2B5A400412a4840B866d95",
-    abi: YieldYakAaveAVAXAbi,
+    abi: YieldYakLpTokenAbi,
     tokenToFetch: "AVAX",
   },
   YY_PTP_sAVAX: {
     address: "0xb8f531c0d3c53B1760bcb7F57d87762Fd25c4977",
-    abi: YYPangolinSAVAXAbi,
+    abi: YieldYakLpTokenAbi,
     tokenToFetch: "sAVAX",
   },
   YY_TJ_AVAX_USDC_LP: {
     address: "0xDEf94a13fF31FB6363f1e03bF18fe0F59Db83BBC",
-    abi: YYTraderJoeLPTokenAbi,
+    abi: YieldYakLpTokenAbi,
     tokenToFetch: "TJ_AVAX_USDC_LP",
   },
   YY_PNG_AVAX_USDC_LP: {
     address: "0xC0cd58661b68e10b49D3Bec4bC5E44e7A7c20656",
-    abi: YYPangolinLPTokenAbi,
+    abi: YieldYakLpTokenAbi,
     tokenToFetch: "PNG_AVAX_USDC_LP",
   },
   YY_PNG_AVAX_ETH_LP: {
     address: "0xFCD2050E213cC54db2c9c99632AC870574FbC261",
-    abi: YYPangolinLPTokenAbi,
+    abi: YieldYakLpTokenAbi,
     tokenToFetch: "PNG_AVAX_ETH_LP",
   },
   YY_TJ_AVAX_sAVAX_LP: {
     address: "0x22EDe03f1115666CF05a4bAfafaEe8F43D42cD56",
-    abi: YYTraderJoeLPTokenAbi,
+    abi: YieldYakLpTokenAbi,
     tokenToFetch: "TJ_AVAX_sAVAX_LP",
   },
   YY_TJ_AVAX_ETH_LP: {
     address: "0x5219558ee591b030E075892acc41334A1694fd8A",
-    abi: YYTraderJoeLPTokenAbi,
+    abi: YieldYakLpTokenAbi,
     tokenToFetch: "TJ_AVAX_ETH_LP",
   },
   YY_GLP: {
     address: "0x9f637540149f922145c06e1aa3f38dcdc32aff5c",
-    abi: YYGLPTokenAbi,
+    abi: YieldYakLpTokenAbi,
     tokenToFetch: "GLP",
   },
 };
--- packages/oracle-node/test/fetchers/arbitrum-evm.spec.ts
+++ packages/oracle-node/test/fetchers/arbitrum-evm.spec.ts
@@ -10,6 +10,8 @@ import {
 } from "../../src/db/local-db";
 import { beefyContractsDetails } from "../../src/fetchers/evm-chain/arbitrum/evm-fetcher/sources/beefy/beefyContractsDetails";
 import BeefyVaultAbi from "../../src/fetchers/evm-chain/shared/abis/BeefyVault.abi.json";
+import { yieldYakTokensContractsDetails } from "../../src/fetchers/evm-chain/arbitrum/evm-fetcher/sources/yield-yak/yieldYakTokensContractsDetails";
+import YieldYakLPTokenAbi from "../../src/fetchers/evm-chain/shared/abis/YieldYakLPToken.abi.json";
 
 jest.setTimeout(15000);
 
@@ -54,6 +56,40 @@ describe("Arbitrum EVM fetcher", () => {
 
       const result = await fetcher.fetchAll(["MOO_GMX"]);
       expect(result).toEqual([{ symbol: "MOO_GMX", value: 38.02298596967315 }]);
+    });
+  });
+
+  describe("YieldYak LP Token - YY_WOMBEX_USDT", () => {
+    beforeAll(async () => {
+      provider = new MockProvider();
+      const [wallet] = provider.getWallets();
+      const yieldYakContract = await deployMockContract(
+        wallet,
+        YieldYakLPTokenAbi
+      );
+      await yieldYakContract.mock.totalDeposits.returns("400488970");
+      await yieldYakContract.mock.totalSupply.returns("400000000");
+
+      multicallContract = await deployMulticallContract(wallet);
+
+      yieldYakTokensContractsDetails.YY_WOMBEX_USDT.address =
+        yieldYakContract.address;
+    });
+
+    test("Should properly fetch data", async () => {
+      const fetcher = new EvmFetcher(
+        "arbitrum-evm-test-fetcher",
+        { mainProvider: provider },
+        multicallContract.address,
+        requestHandlers
+      );
+
+      await saveMockPriceInLocalDb(0.99, "USDT");
+
+      const result = await fetcher.fetchAll(["YY_WOMBEX_USDT"]);
+      expect(result).toEqual([
+        { symbol: "YY_WOMBEX_USDT", value: 0.99121020075 },
+      ]);
     });
   });
 });
