0861721299886fd0bec942c46c39a381d3b036ea
Cezary Haliniarz <cezary.haliniarz@gmail.com>
2023-08-30 17:18:16 +0200

    feat: add MOO_DPX_ETH_LP token (#1023)

--- packages/oracle-node/src/fetchers/evm-chain/arbitrum/evm-fetcher/sources/beefy/beefyContractsDetails.ts
+++ packages/oracle-node/src/fetchers/evm-chain/arbitrum/evm-fetcher/sources/beefy/beefyContractsDetails.ts
@@ -3,4 +3,8 @@ export const beefyContractsDetails = {
     address: "0x5B904f19fb9ccf493b623e5c8cE91603665788b0",
     tokenToFetch: "GMX",
   },
+  MOO_SUSHI_DPX_ETH_LP: {
+    address: "0x4D323f77c32EDdC62BF8eAbA11E5C573FD0a2ccd",
+    tokenToFetch: "SUSHI_DPX_ETH_LP",
+  },
 };
--- packages/oracle-node/test/fetchers/arbitrum-evm.spec.ts
+++ packages/oracle-node/test/fetchers/arbitrum-evm.spec.ts
@@ -2,7 +2,11 @@ import { Contract } from "ethers";
 import { MockProvider, deployMockContract } from "ethereum-waffle";
 import { EvmFetcher } from "../../src/fetchers/evm-chain/shared/EvmFetcher";
 import { requestHandlers } from "../../src/fetchers/evm-chain/arbitrum/evm-fetcher/sources";
-import { deployMulticallContract, saveMockPriceInLocalDb } from "./_helpers";
+import {
+  deployMulticallContract,
+  saveMockPriceInLocalDb,
+  saveMockPricesInLocalDb,
+} from "./_helpers";
 import {
   clearPricesSublevel,
   closeLocalLevelDB,
@@ -12,6 +16,8 @@ import { beefyContractsDetails } from "../../src/fetchers/evm-chain/arbitrum/evm
 import BeefyVaultAbi from "../../src/fetchers/evm-chain/shared/abis/BeefyVault.abi.json";
 import { yieldYakTokensContractsDetails } from "../../src/fetchers/evm-chain/arbitrum/evm-fetcher/sources/yield-yak/yieldYakTokensContractsDetails";
 import YieldYakLPTokenAbi from "../../src/fetchers/evm-chain/shared/abis/YieldYakLPToken.abi.json";
+import { dexLpTokensContractsDetails } from "../../src/fetchers/evm-chain/arbitrum/evm-fetcher/sources/dex-lp-tokens/dexLpTokensContractsDetails";
+import DexLpTokenAbi from "../../src/fetchers/evm-chain/shared/abis/DexLpToken.abi.json";
 
 jest.setTimeout(15000);
 
@@ -89,6 +95,47 @@ describe("Arbitrum EVM fetcher", () => {
       const result = await fetcher.fetchAll(["YY_WOMBEX_USDT"]);
       expect(result).toEqual([
         { symbol: "YY_WOMBEX_USDT", value: 0.99121020075 },
+      ]);
+    });
+  });
+
+  describe("DEX LP Token - SUSHI_DPX_ETH_LP", () => {
+    beforeAll(async () => {
+      provider = new MockProvider();
+      const [wallet] = provider.getWallets();
+
+      const dexLpTokenContract = await deployMockContract(
+        wallet,
+        DexLpTokenAbi
+      );
+      await dexLpTokenContract.mock.getReserves.returns(
+        "22695143440192357835227",
+        "1047489210335175121050",
+        1693378156
+      );
+      await dexLpTokenContract.mock.totalSupply.returns(
+        "4354205571809301622184"
+      );
+
+      multicallContract = await deployMulticallContract(wallet);
+
+      dexLpTokensContractsDetails.SUSHI_DPX_ETH_LP.address =
+        dexLpTokenContract.address;
+    });
+
+    test("Should properly fetch data", async () => {
+      const fetcher = new EvmFetcher(
+        "arbitrum-evm-test-fetcher",
+        { mainProvider: provider },
+        multicallContract.address,
+        requestHandlers
+      );
+
+      await saveMockPricesInLocalDb([79.61, 1720.62], ["DPX", "ETH"]);
+
+      const result = await fetcher.fetchAll(["SUSHI_DPX_ETH_LP"]);
+      expect(result).toEqual([
+        { symbol: "SUSHI_DPX_ETH_LP", value: 828.874171569813 },
       ]);
     });
   });
