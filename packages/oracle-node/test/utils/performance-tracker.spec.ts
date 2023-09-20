c025ea99e9cdbabb7154e2eaf7f438bf817dcc9c
Wojtek Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2023-09-20 16:36:51 +0200

    fix: fix tests

--- packages/oracle-node/test/utils/performance-tracker.spec.ts
+++ packages/oracle-node/test/utils/performance-tracker.spec.ts
@@ -8,28 +8,17 @@ import { config as nodeConfig } from "../../src/config";
 
 describe("performance-tracker", () => {
   test("should queue telemetry when valid config", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (nodeConfig as any) = {
+      ...nodeConfig,
+      telemetryUrl: "testUrl",
+      telemetryAuthorizationToken: "testToken",
+    };
     await expect(queueNodeTelemetry()).resolves.not.toThrowError();
     expect(isTelemetryEnabled()).toBe(true);
   });
 
   test("should end track with queue telemetry when valid config", () => {
-    const trackingId = trackStart("testLabel");
-    trackEnd(trackingId);
-    expect(isTelemetryEnabled()).toBe(true);
-  });
-
-  test("should not throw error when fail to send telemetry", async () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (nodeConfig as any) = {
-      ...nodeConfig,
-      telemetryUrl: "testUrl",
-      telemetryAuthorizationToken: "testToken",
-    };
-    await expect(queueNodeTelemetry()).resolves.not.toThrowError();
-    expect(isTelemetryEnabled()).toBe(true);
-  });
-
-  test("should end track without queue telemetry when invalid config", () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (nodeConfig as any) = {
       ...nodeConfig,
@@ -41,13 +30,26 @@ describe("performance-tracker", () => {
     expect(isTelemetryEnabled()).toBe(true);
   });
 
-  test("should return false when no telemetry config", () => {
+  test("should do nothing when no telemetry config", async () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (nodeConfig as any) = {
       ...nodeConfig,
       telemetryUrl: "",
       telemetryAuthorizationToken: "",
     };
+    await expect(queueNodeTelemetry()).resolves.not.toThrowError();
+    expect(isTelemetryEnabled()).toBe(false);
+  });
+
+  test("should end track without queue telemetry when no telemetry config", () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (nodeConfig as any) = {
+      ...nodeConfig,
+      telemetryUrl: "",
+      telemetryAuthorizationToken: "",
+    };
+    const trackingId = trackStart("testLabel");
+    trackEnd(trackingId);
     expect(isTelemetryEnabled()).toBe(false);
   });
 });
